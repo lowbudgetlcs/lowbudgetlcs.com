2aa4c54cf2c2ffc4865f0c51fe35fb07ba600877
Mamii868 <marcmotenii@gmail.com>
2024-07-17 11:58:35 -0500

    Fix tailwind content and add colors
    Yes I did skip over that in the docs and wonder why it was not working for 10 minutes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
       'orange': '#e8a012',
-      'light-orange': '#da9b22'
+      'light-orange': '#da9b22',
+      'gray': '#f4f4f4',
+      'white': '#ffffff',
+      'black': '#1a1a1a',
     },
     fontFamily: {},
     extend: {},
