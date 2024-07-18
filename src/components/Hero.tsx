4160b556a03d4ac9fe3290ea935bf58efdcf6fc6
Mamii868 <marcmotenii@gmail.com>
2024-07-17 23:44:28 -0500

    Change hero to be shorter

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,12 +4,12 @@ import video from '../assets/LeagueVideoPlaceholder.webm'
 function Hero() {
   return (
     <div className="hero w-full h-lvh">
-        <div className="text-white heroText flex flex-col gap-6 items-center justify-center h-full text-center z-[2]">
+        <div className="text-white heroText flex flex-col gap-6 items-center justify-center h-4/6 text-center z-[2]">
             <h1 className="text-4xl font-bold">Welcome to the Low Budget LCS</h1>
             <p className="text-lg">Competitive League of Legends for the low price of free</p>
         </div>
-        <video className="heroVideo w-full h-full object-cover absolute top-0 left-0 z-[-2]" muted loop><source src={video}></source> </video>
-        <div className='overlay w-full h-full bg-black opacity-40 absolute top-0 left-0 z-[-1]'></div>
+        <video className="heroVideo w-full h-5/6 object-cover absolute top-0 left-0 z-[-2] overflow-hidden" autoPlay muted loop><source src={video}></source> </video>
+        <div className='overlay w-full h-5/6 bg-black opacity-50 absolute top-0 left-0 z-[-1]'></div>
     </div>
   );
 }
