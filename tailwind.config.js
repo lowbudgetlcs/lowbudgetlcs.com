3d3221cdafcc99467f28882a2f6dd9d23e6ac9c0
Mamii868 <marcmotenii@gmail.com>
2024-07-20 17:07:25 -0500

    Add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,18 @@ export default {
       'orange': '#e8a012',
       'light-orange': '#da9b22',
       'gray': '#f4f4f4',
-      'white': '#ffffff',
+      'white': '#F4F5FB',
       'black': '#1a1a1a',
       'light-blue': '#d9dbf1',
+      'dark-blue': '#1B1E48',
     },
     fontFamily: {},
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(ellipse, rgba(0,0,0,0) 48%, rgba(0,0,0,100) 76%)'
+    },
+    height: {
+      '47': '47rem',
+    },
     extend: {},
   },
   plugins: [],
