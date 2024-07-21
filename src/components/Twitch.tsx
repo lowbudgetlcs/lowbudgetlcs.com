13e393bf2dda8ff2558b945523344d1cff525407
Mamii868 <marcmotenii@gmail.com>
2024-07-20 19:03:38 -0500

    Change parent url
    Having it without invalid characters causes about 8 errors to pop up due to it being hosted locally temporarily

--- src/components/Twitch.tsx
+++ src/components/Twitch.tsx
@@ -1,8 +1,9 @@
 function Twitch() {
+  //! Change the parent attribute before deploying to production
   return (
     <div className="flex justify-center items-center py-8">
       <iframe
-        src="https://player.twitch.tv/?lowbudgetlcs&parent=lowbudgetlcs.com"
+        src="https://player.twitch.tv/?lowbudgetlcs&parent=http://localhost/anything"
         height="480"
         width="720"
         allowFullScreen
