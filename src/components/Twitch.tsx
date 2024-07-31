522483c2a866e68a2f5e41551c50f25886a97ad4
Mamii868 <marcmotenii@gmail.com>
2024-07-31 16:29:02 -0500

    Add title to twitch component

--- src/components/Twitch.tsx
+++ src/components/Twitch.tsx
@@ -1,13 +1,17 @@
 function Twitch() {
   //! Change the parent attribute before deploying to production
   return (
-    <div className="flex justify-center items-center py-8">
-      <iframe
-        src="https://player.twitch.tv/?lowbudgetlcs&parent=http://localhost/anything"
-        height="480"
-        width="720"
-        allowFullScreen
-      ></iframe>
+    <div>
+      <h2 className="text-4xl text-center font-semibold p-2">Twitch Stream</h2>
+
+      <div className="flex justify-center items-center py-8">
+        <iframe
+          src="https://player.twitch.tv/?lowbudgetlcs&parent=http://localhost/anything"
+          height="480"
+          width="720"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
