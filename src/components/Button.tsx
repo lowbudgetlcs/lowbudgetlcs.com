73dd9eb9e96cf0a51e2c40282328ad8126d75722
Mamii868 <marcmotenii@gmail.com>
2024-08-07 16:50:29 -0500

    Add new summary component
    I messed up and reverted 8 different commits for this

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 const Button = ({ children}: {children: string}) => {
     return (
-<a href="#_" className="relative flex justify-center px-24 py-8 overflow-hidden group">
-<span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-orange group-hover:w-full ease"></span>
-<span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-orange group-hover:w-full ease"></span>
-<span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 border-l-2  border-orange group-hover:h-full ease"></span>
-<span className="absolute bottom-0 right-0 w-full h-0 transition-all duration-300 border-r-2 border-orange group-hover:h-full ease"></span>
+<a href="#_" className="relative bg-light-blue/20 flex justify-center px-8 py-2 overflow-hidden group rounded-md">
+<span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-4 border-orange group-hover:w-full rounded-md ease"></span>
+<span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-4 border-orange group-hover:w-full ease"></span>
+<span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 border-l-4  border-orange group-hover:h-full ease"></span>
+<span className="absolute bottom-0 right-0 w-full h-0 transition-all duration-300 border-r-4 border-orange group-hover:h-full ease"></span>
 <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">{children}</span>
 </a>
     );
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -3,14 +3,25 @@ import Button from "./Button";
 function Summary() {
   return (
     <div className="summary p-8 flex flex-col items-center">
-      <h2 className="text-4xl text-center font-semibold p-2">LBLCS</h2>
-      <p className="py-2 text-xl px-0 md:px-20 lg:px-48 xl:px-80">
-        The LowBudget LCS is a competitive, free, and friendly amateur rec
-        league. We have 4 leagues that range across all skill levels. We provide
-        an experience that mirrors what would happen if Riot Games suffered a
-        catastrophic financial disaster. Season 13 is starting in the fall! Our
-        Discord has update
-      </p>
+      <div className="flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40">
+        <div className="w-full h-96 bg-cover bg-center text-center bg-[url('src/assets/summaryImg.jpg')] border-b-orange border-b-4 md:border-r-orange md:border-r-4"></div>
+        <div className=" w-full md:w-1/2 p-2 md:p-4">
+          <h2 className="text-xl text-center font-semibold p-2">
+            Amateurs Unite!
+          </h2>
+          <p className="py-2 text-md">
+            The LowBudget LCS is a competitive, free, and friendly amateur rec
+            league. We have 4 leagues that range across all skill levels. We
+            provide an experience that mirrors what would happen if Riot Games
+            suffered a catastrophic financial disaster. Season 13 is starting in
+            the fall!
+          </p>
+          <div className="font-semibold">
+            <Button>Join The Discord</Button>
+          </div>
+        </div>
+      </div>
+
       <div className="btnList grid sm:grid-cols-2 gap-2 sm:gap-8 py-2 font-bold text-4xl px-0 md:px-10 lg:px-24 xl:px-40">
         <Button>About</Button>
         <Button>Rules</Button>
