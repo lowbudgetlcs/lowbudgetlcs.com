19c7fac821d13173d2d81d717174f9af27a7696a
Mamii868 <marcmotenii@gmail.com>
2024-08-07 17:42:26 -0500

    Remove twitch embed temporarily
    Planning on having this be a popup when live on twitch

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,12 @@
 import Hero from "./Hero";
 import Summary from "./Summary";
-import Twitch from "./Twitch";
+// import Twitch from "./Twitch";
 
 function Home() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white">
       <Hero />
       <Summary />
-      <Twitch />
     </div>
   );
 }
