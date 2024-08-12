97084c455641c47b102d84759bf939daffaa1e5d
Mamii868 <marcmotenii@gmail.com>
2024-08-12 10:17:15 -0500

    Refactor home cards
    Planning on using some form of these elsewhere and it was too cluttered to look at all the time.

---
+++ src/components/Card.tsx
@@ -0,0 +1,70 @@
+import { NavLink } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import Button from "./Button";
+
+interface CardProps {
+  link: string;
+  title: string;
+  text: string;
+  btnText: string;
+  image: string;
+  reverse: boolean;
+}
+
+function Card({ title, text, link, btnText, image, reverse }: CardProps) {
+  const [ref, inView] = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
+  if (!reverse)
+    return (
+      <div
+        ref={ref}
+        className={`card ${
+          inView ? "translate-x-0 opacity-100" : "translate-x-20 opacity-0"
+        } transition duration-700 flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40`}
+      >
+        <div
+          className={`w-full h-96 bg-cover bg-center text-center bg-[url('${image}')] border-b-orange border-b-4 md:border-r-orange md:border-r-4`}
+        ></div>
+        <div className=" w-full md:w-1/2 p-2 md:p-4">
+          <h2 className="text-xl text-white text-center font-semibold p-2">
+            {title}
+          </h2>
+          <p className="py-2 text-sm md:text-md">{text}</p>
+          <div className="font-semibold">
+            <NavLink to={link}>
+              <Button>{btnText}</Button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    );
+  else
+    return (
+      <div
+        ref={ref}
+        className={`card ${
+          inView ? "translate-x-0 opacity-100" : "-translate-x-20 opacity-0"
+        } transition duration-700 flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40`}
+      >
+        <div className=" w-full md:w-1/2 p-2 md:p-4">
+          <h2 className="text-xl text-white text-center font-semibold p-2">
+            {title}
+          </h2>
+          <p className="py-2 text-sm md:text-md">
+           {text}
+          </p>
+          <div className="font-semibold mt-4">
+            <NavLink target="_blank" to={link}>
+              <Button>{btnText}</Button>
+            </NavLink>
+          </div>
+        </div>
+        <div className={`w-full h-96 bg-cover bg-center text-center bg-[url('${image}')] border-t-orange border-t-4 md:border-l-orange md:border-l-4`}></div>
+      </div>
+    );
+}
+
+export default Card;
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,84 +1,66 @@
-import { NavLink } from "react-router-dom";
-import { useInView } from "react-intersection-observer";
-import Button from "./Button";
+import Card from "./Card";
 
 function Summary() {
-
-  const [ ref, inView ] = useInView({
-    threshold: 0.3,
-    triggerOnce: true,
-  });
-  const [ ref1, inView1 ] = useInView({
-    threshold: 0.3,
-    triggerOnce: true,
-  });
-  const [ ref2, inView2 ]= useInView({
-    threshold: 0.3,
-    triggerOnce: true,
-  });
-  return (
-    <div className="summary p-8 flex flex-col items-center -mt-28 text-white/60">
-      <div className="cardContainer flex flex-col gap-16 z-10 overflow-hidden">
-        <div ref={ref} className={`card ${inView ? "translate-x-0 opacity-100" : "translate-x-20 opacity-0"} transition duration-700 flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40`}>
-          <div className="w-full h-96 bg-cover bg-center text-center bg-[url('src/assets/summaryImg.jpg')] border-b-orange border-b-4 md:border-r-orange md:border-r-4"></div>
-          <div className=" w-full md:w-1/2 p-2 md:p-4">
-            <h2 className="text-xl text-white text-center font-semibold p-2">
-              Amateurs Unite!
-            </h2>
-            <p className="py-2 text-sm md:text-md">
-              The LowBudget LCS is a competitive, free, and friendly amateur rec
+  const aboutCard = {
+    title: "Amateurs Unite!",
+    text: `The LowBudget LCS is a competitive, free, and friendly amateur rec
               league. We have 4 leagues that range across all skill levels. We
               provide an experience that mirrors what would happen if Riot Games
               suffered a catastrophic financial disaster. Season 13 is starting
-              in the fall!
-            </p>
-            <div className="font-semibold">
-              <NavLink to="/about">
-                <Button>Learn More</Button>
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        <div ref={ref1} className={`card ${inView1 ? "translate-x-0 opacity-100" : "-translate-x-20 opacity-0"} transition duration-700 flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40`}>
-          <div className=" w-full md:w-1/2 p-2 md:p-4">
-            <h2 className="text-xl text-white text-center font-semibold p-2">
-              Watch Games Live
-            </h2>
-            <p className="py-2 text-sm md:text-md">
-              Watch matches from all brackets live on Twitch! Cheer on your
+              in the fall!`,
+    link: "/about",
+    btnText: "Learn More",
+    image: "src/assets/summaryImg.jpg",
+  };
+
+  const twitchCard = {
+    title: "Watch Games Live",
+    text: `Watch matches from all brackets live on Twitch! Cheer on your
               favorite team as they win big (or int big) while interacting with
-              both the casters and community. Earn all the Schmeckles you can!
-            </p>
-            <div className="font-semibold mt-4">
-              <NavLink
-                target="_blank"
-                to={"https://www.twitch.tv/lowbudgetlcs"}
-              >
-                <Button>Watch Here</Button>
-              </NavLink>
-            </div>
-          </div>
-          <div className="w-full h-96 bg-cover bg-center text-center bg-[url('src/assets/twitchPhone.jpg')] border-t-orange border-t-4 md:border-l-orange md:border-l-4"></div>
-        </div>
-        <div ref={ref2} className={`card ${inView2 ? "translate-x-0 opacity-100" : "translate-x-20 opacity-0"} transition duration-700 flex flex-col md:flex-row items-center max-w-md md:max-w-4xl min-h-72 md:h-80 overflow-hidden gap-4 rounded-lg bg-gray/40`}>
-          <div className="w-full h-96 bg-cover bg-center text-center bg-[url('src/assets/smilingGamer.jpg')] border-b-orange border-b-4 md:border-r-orange md:border-r-4"></div>
-          <div className=" w-full md:w-1/2 p-2 md:p-4">
-            <h2 className="text-xl text-white text-center font-semibold p-2">
-              Have a Blast with Friends
-            </h2>
-            <p className="py-2 text-sm md:text-md">
-              The LBLCS is a community where players of all ranks can meet
+              both the casters and community. Earn all the Schmeckles you can!`,
+    link: "https://www.twitch.tv/lowbudgetlcs",
+    btnText: "Watch Here",
+    image: "src/assets/twitchPhone.jpg",
+  };
+
+  const discordCard = {
+    title: "Have a Blast with Friends",
+    text: `The LBLCS is a community where players of all ranks can meet
               awesome people, play organized League of Legends, and find a
               welcome environment for just about everything! Show off your pets,
-              share cool memes, and talk it up with the community!
-            </p>
-            <div className="font-semibold">
-              <NavLink target="_blank" to={"https://discord.gg/lblcs"}>
-                <Button>Join The Discord</Button>
-              </NavLink>
-            </div>
-          </div>
-        </div>
+              share cool memes, and talk it up with the community!`,
+    link: "https://discord.gg/lblcs",
+    btnText: "Join The Discord",
+    image: "src/assets/smilingGamer.jpg",
+  }
+
+  return (
+    <div className="summary p-8 flex flex-col items-center -mt-28 text-white/60">
+      <div className="cardContainer flex flex-col gap-16 z-10 overflow-hidden">
+        <Card
+          reverse={false}
+          title={aboutCard.title}
+          text={aboutCard.text}
+          link={aboutCard.link}
+          btnText={aboutCard.btnText}
+          image={aboutCard.image}
+        />
+        <Card
+          reverse
+          title={twitchCard.title}
+          text={twitchCard.text}
+          link={twitchCard.link}
+          btnText={twitchCard.btnText}
+          image={twitchCard.image}
+        />
+        <Card
+          reverse={false}
+          title={discordCard.title}
+          text={discordCard.text}
+          link={discordCard.link}
+          btnText={discordCard.btnText}
+          image={discordCard.image}
+        />
       </div>
     </div>
   );
