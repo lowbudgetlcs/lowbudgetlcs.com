dec1070bcbfcc29364c543758cfb69c60b5afdd5
Mamii868 <marcmotenii@gmail.com>
2024-08-12 09:21:28 -0500

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import './App.css'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./components/About";
+
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+      <App />
+  </React.StrictMode>
+);
