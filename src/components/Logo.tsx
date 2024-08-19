60542258d33197f327b8be777d97eb60f48cadcc
Mamii868 <marcmotenii@gmail.com>
2024-08-19 09:47:41 -0500

    Add visibility for light mode

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ function Logo() {
   return (
     <div className="logo scale-125">
       {/* <NavLink to="#" className='text-white text-4xl font-bold'>Low Budget LCS</NavLink> */}
-      <img src={logo} alt="logo" width={116} height={66}/>
+      <img src={logo} alt="logo" width={116} height={66} className="dark:bg-transparent bg-black/50 rounded-lg"/>
     </div>
   );
 }
