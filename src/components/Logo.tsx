4263042786d4792c7f8dc068e5e55714d0399128
Mamii868 <marcmotenii@gmail.com>
2024-08-20 12:21:41 -0500

    Fix logo not going to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,17 @@
-import logo from "../assets/Transparent_LBLCS_Logo_White_Text.png"
+import { NavLink } from "react-router-dom";
+import logo from "../assets/Transparent_LBLCS_Logo_White_Text.png";
 function Logo() {
   return (
     <div className="logo scale-125">
-      {/* <NavLink to="#" className='text-white text-4xl font-bold'>Low Budget LCS</NavLink> */}
-      <img src={logo} alt="logo" width={116} height={66} className="dark:bg-transparent bg-black/50 rounded-lg"/>
+      <NavLink to={"/"}>
+        <img
+          src={logo}
+          alt="logo"
+          width={116}
+          height={66}
+          className="dark:bg-transparent bg-black/50 rounded-lg"
+        />
+      </NavLink>
     </div>
   );
 }
