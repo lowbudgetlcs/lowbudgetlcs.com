30b3d5023d12fa83358ee1b18b2ef7674b5dae59
Mamii868 <marcmotenii@gmail.com>
2024-08-19 23:31:39 -0500

    Fix autoplay on IOS

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,9 +15,10 @@ function Hero() {
         className="mt-[-1.5rem] heroVideo w-full h-[40rem] md:h-[47rem] object-cover absolute md:scale-110 lg:translate-y-8 top-0 left-0 z-[1] overflow-hidden"
         autoPlay
         muted
+        playsInline
         loop
       >
-        <source src={video}></source>
+        <source src={video} type="video/mp4"></source>
       </video>
       <div className="overlay w-full h-[22rem] md:h-[16rem] bg-gradient-to-b from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute bottom-0 left-0 z-[2]"></div>
     </div>
