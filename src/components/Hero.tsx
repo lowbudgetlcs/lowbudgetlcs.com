3e0636fd3f467ca8a51336247b7e0681a1f3a7b9
Mamii868 <marcmotenii@gmail.com>
2024-08-19 19:26:13 -0500

    Change video size on smaller screens
    The logo would not fit on mobile screens on the video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,14 @@ function Hero() {
   return (
     <div id="hero" className="hero relative w-full overflow-hidden h-[47rem]">
       <video
-        className="mt-[-1.5rem] heroVideo w-full h-[47rem] object-cover absolute scale-110 lg:translate-y-8 top-0 left-0 z-[1] overflow-hidden"
+        className="mt-[-1.5rem] heroVideo w-full h-[40rem] md:h-[47rem] object-cover absolute md:scale-110 lg:translate-y-8 top-0 left-0 z-[1] overflow-hidden"
         autoPlay
         muted
         loop
       >
         <source src={video}></source>
       </video>
-      <div className="overlay w-full h-[16rem] bg-gradient-to-b from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute bottom-0 left-0 z-[2]"></div>
+      <div className="overlay w-full h-[22rem] md:h-[16rem] bg-gradient-to-b from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute bottom-0 left-0 z-[2]"></div>
     </div>
   );
 }
