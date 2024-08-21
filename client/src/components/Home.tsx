60e84b64edf6137b7b877d9dbc38542eb371633b
Mamii868 <marcmotenii@gmail.com>
2024-08-20 22:30:00 -0500

    Add twitch component to home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from "./Hero";
 import Summary from "./Summary";
+import Twitch from "./Twitch";
 // import Twitch from "./Twitch";
 
 function Home() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white">
+      <Twitch/>
       <Hero />
       <Summary />
     </div>
