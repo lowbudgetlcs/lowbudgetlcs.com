5f9057d994226aefe080e80b061bb69eccac64a5
Mamii868 <marcmotenii@gmail.com>
2024-08-20 19:51:47 -0500

    Add route to check if live

--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const axios = require('axios');
+const { error } = require("console");
 const app = express();
 const port = process.env.PORT;
 
-app.get("/api", (req, res) => {
-    res.json({})
-});
-
-app.listen(port, () => {
-    console.log("Server started on port " + port)
-});
-
-
-
 const clientSecret = process.env.CLIENT_SECRET
 const clientID = process.env.CLIENT_ID
+
+app.get("/api/checklive", async (req, res) => {
+    try {
+        const accessToken = await getTwitchToken(clientID, clientSecret);
+        const isLive = await checkIfLive(clientID, accessToken);
+        console.log(isLive)
+        res.json({ isLive })
+    } catch (err) {
+        console.error("ERROR: " + err)
+        res.json({ err })
+    }
+});
+
 
 let twitchToken;
 let tokenExpiration;
 
 async function getTwitchToken(clientID, clientSecret) {
+    console.log(clientID
+
+    )
     try {
         if (!twitchToken || new Date() >= tokenExpiration) {
             const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
@@ -36,9 +43,6 @@ async function getTwitchToken(clientID, clientSecret) {
 
             console.log('New token acquired:', twitchToken);
             console.log('Token expiration date:', tokenExpiration);
-        } else {
-            console.log('Using cached token:', twitchToken);
-            console.log('Token expiration date:', tokenExpiration);
         }
         return twitchToken;
     } catch (err) {
@@ -46,4 +50,31 @@ async function getTwitchToken(clientID, clientSecret) {
     }
 }
 
-getTwitchToken(clientID, clientSecret);
+async function checkIfLive(clientID, accessToken) {
+    let isLive
+    try {
+
+        const response = await axios.get('https://api.twitch.tv/helix/streams', {
+            headers: {
+                'Client-ID': clientID,
+                'Authorization': `Bearer ${accessToken}`
+            },
+            params: {
+                user_login: 'lowbudgetlcs'
+            }
+        });
+        if (response.data.data.length > 0) {
+            isLive = true
+        } else {
+            isLive = false
+
+        }
+        return isLive
+    } catch (err) {
+        console.error('ERROR:', err)
+    }
+}
+
+app.listen(port, () => {
+    console.log("Server started on port " + port)
+});
