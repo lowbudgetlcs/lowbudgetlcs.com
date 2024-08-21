b1e3b0828ef56fa1dd670f2e490ae56461a8366a
Mamii868 <marcmotenii@gmail.com>
2024-08-20 22:28:44 -0500

    Add call to server
    Not the popup yet but only displays twitch stream if live

--- client/src/components/Twitch.tsx
+++ client/src/components/Twitch.tsx
@@ -1,41 +1,46 @@
+const url = import.meta.env.VITE_URL || 'https://lowbudgetlcs.com/api/checklive'
 
 function Twitch() {
-const twitchKey = import.meta.env.VITE_TWITCH_CLIENT
-const authToken = import.meta.env.VITE_TWITCH_AUTH
-
-  async function checkIfLive() {
+  let isLive;
+  const checkIfLive = async () => {
     try {
-      const headers = {
-        "Client-Id": twitchKey,
-        "Authorization": `Bearer ${authToken}`
-    };
-      const response = await fetch(`https://api.twitch.tv/helix/streams&user_login=lowbudgetlcs`, {
-        method: 'GET',
-        headers});
-      const data = await response.json();
-      console.log(data)
+      const response = await fetch(url, {
+        method: "GET",
+      });
 
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      isLive = data.isLive
+      return isLive;
     } catch (error) {
-      console.log("Error occurred:", error);
+      console.error("Error occurred:", error);
     }
-  }
+  };
 
   checkIfLive();
   //! Change the parent attribute before deploying to production
-  return (
-    <div>
-      <h2 className="text-4xl text-center font-semibold p-2">Twitch Stream</h2>
-
-      <div className="flex justify-center items-center py-8">
-        <iframe
-          src="https://player.twitch.tv/?channel=lowbudgetlcs&parent=localhost"
-          height="480"
-          width="720"
-          allowFullScreen
-        ></iframe>
+  if (isLive) {
+    return (
+      <div>
+        <h2 className="text-4xl text-center font-semibold p-2">Twitch Stream</h2>
+  
+        <div className="flex justify-center items-center py-8">
+          <iframe
+            src="https://player.twitch.tv/?channel=lowbudgetlcs&parent=localhost"
+            height="480"
+            width="720"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return
+  }
+
 }
 
 export default Twitch;
