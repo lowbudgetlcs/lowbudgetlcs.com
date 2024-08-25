1c00bde8210bfd54787e3519ae6effee86e377e4
Mamii868 <marcmotenii@gmail.com>
2024-08-25 17:46:24 -0500

    Add readability

--- server/db/queries/select.ts
+++ server/db/queries/select.ts
@@ -1,13 +1,14 @@
-import {db} from '../index'
-import { playersTable, SelectPlayer } from '../schema'
+import { db } from "../index";
+import { playersTable } from "../schema";
 
 export async function getPlayers(): Promise<
-Array<{
+  Array<{
     id: Number;
     primary_riot_puuid: String;
     team_id: Number | null;
     summoner_name: String;
-}>
+  }>
 > {
-    return db.select().from(playersTable)
+  const allPlayers = db.select().from(playersTable);
+  return allPlayers;
 }
