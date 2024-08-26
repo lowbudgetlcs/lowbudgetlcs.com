91bb4eaa2f43549833048fc10c0743766deb840b
Mamii868 <marcmotenii@gmail.com>
2024-08-26 11:16:47 -0500

    Rename table columns

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -2,17 +2,17 @@ import { char, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const playersTable = pgTable("players", {
   id: integer("id").primaryKey(),
-  primary_riot_puuid: char("primary_riot_puuid").notNull(),
-  team_id: integer("team_id"),
-  summoner_name: varchar("summoner_name").notNull(),
+  primaryRiotPuuid: char("primary_riot_puuid").notNull(),
+  teamId: integer("team_id"),
+  summonerName: varchar("summoner_name").notNull(),
 });
 
 export const teamsTable = pgTable("teams", {
   id: integer("id").primaryKey(),
   teamName: varchar("name").notNull(),
-  division: integer("division").notNull(),
+  divisionId: integer("division").notNull(),
   groupId: char("group_id").notNull(),
-  captainID: integer("captain_id"),
+  captainId: integer("captain_id"),
   logo: varchar("logo"),
 });
 
