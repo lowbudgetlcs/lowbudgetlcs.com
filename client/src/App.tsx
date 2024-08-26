b2b4c68d22d8d886c196fba94157392afca343d9
Mamii868 <marcmotenii@gmail.com>
2024-08-26 12:19:47 -0500

    Add comment reminder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 import ScrollToTop from "./components/ScrollToTop";
+//! Enable Twitch on prod
 import Twitch from "./components/Twitch";
 import Roster from "./components/Roster";
 
