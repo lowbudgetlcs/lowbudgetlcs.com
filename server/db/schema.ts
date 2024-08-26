31e5d823336a7fe9ae0953da559f14a9b3049093
Mamii868 <marcmotenii@gmail.com>
2024-08-26 10:56:39 -0500

    Add teams schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,12 +1,21 @@
-import { serial } from "drizzle-orm/mysql-core";
-import { char, integer, pgTable, primaryKey, varchar } from "drizzle-orm/pg-core";
+import { char, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const playersTable = pgTable('players', {
-    id: integer('id').primaryKey(),
-    primary_riot_puuid: char('primary_riot_puuid').notNull(),
-    team_id: integer('team_id'),
-    summoner_name: varchar('summoner_name').notNull()
-})
+export const playersTable = pgTable("players", {
+  id: integer("id").primaryKey(),
+  primary_riot_puuid: char("primary_riot_puuid").notNull(),
+  team_id: integer("team_id"),
+  summoner_name: varchar("summoner_name").notNull(),
+});
 
+export const teamsTable = pgTable("teams", {
+  id: integer("id").primaryKey(),
+  teamName: varchar("name").notNull(),
+  division: integer("division").notNull(),
+  groupId: char("group_id").notNull(),
+  captainID: integer("captain_id"),
+  logo: varchar("logo"),
+});
 
-export type SelectPlayer =typeof playersTable.$inferSelect
+export type SelectPlayer = typeof playersTable.$inferSelect;
+
+export type SelectTeam = typeof teamsTable.$inferSelect;
