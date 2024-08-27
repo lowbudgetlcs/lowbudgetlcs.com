d338a4da3e4c66ed4e70d9e1b0455659faf0cbeb
Mamii868 <marcmotenii@gmail.com>
2024-08-26 22:50:49 -0500

    Add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,8 @@ import ScrollToTop from "./components/ScrollToTop";
 //! Enable Twitch on prod
 import Twitch from "./components/Twitch";
 import Roster from "./components/Roster";
+import LeagueGroups from "./components/LeagueGroups";
+import LeaguePlayers from "./components/LeaguePlayers";
 
 function App() {
   return (
@@ -20,10 +22,30 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="*" element={<Error />} />
         <Route path="rosters" element={<Roster />}>
-          <Route path="economy" element={<Roster />} />
-          <Route path="commercial" element={<Roster />} />
-          <Route path="financial" element={<Roster />} />
-          <Route path="executive" element={<Roster />} />
+          <Route path="economy" element={<LeagueGroups />}>
+            <Route path="a" element={<LeaguePlayers />} />
+            <Route path="b" element={<LeaguePlayers />} />
+            <Route path="c" element={<LeaguePlayers />} />
+            <Route path="d" element={<LeaguePlayers />} />
+          </Route>
+          <Route path="commercial" element={<LeagueGroups />}>
+            <Route path="a" element={<LeaguePlayers />} />
+            <Route path="b" element={<LeaguePlayers />} />
+            <Route path="c" element={<LeaguePlayers />} />
+            <Route path="d" element={<LeaguePlayers />} />
+          </Route>
+          <Route path="financial" element={<LeagueGroups />}>
+            <Route path="a" element={<LeaguePlayers />} />
+            <Route path="b" element={<LeaguePlayers />} />
+            <Route path="c" element={<LeaguePlayers />} />
+            <Route path="d" element={<LeaguePlayers />} />
+          </Route>
+          <Route path="executive" element={<LeagueGroups />}>
+            <Route path="a" element={<LeaguePlayers />} />
+            <Route path="b" element={<LeaguePlayers />} />
+            <Route path="c" element={<LeaguePlayers />} />
+            <Route path="d" element={<LeaguePlayers />} />
+          </Route>
         </Route>
       </Routes>
       <Footer />
