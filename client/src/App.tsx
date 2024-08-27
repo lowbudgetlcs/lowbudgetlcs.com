4933fa9dc5d2fdd756570d12aa5cac781987a7c0
Mamii868 <marcmotenii@gmail.com>
2024-08-26 23:38:58 -0500

    Enable twitch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <Navbar />
+      <Twitch />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
