6333d9a5105ceee1e5144c60490ec7f76fc2102b
Mamii868 <marcmotenii@gmail.com>
2024-08-27 23:49:39 -0500

    Add display of groups based on group

--- client/src/components/LeagueGroups.tsx
+++ client/src/components/LeagueGroups.tsx
@@ -1,13 +1,44 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
-import { PlayerProps, TeamProps } from "./Roster";
+import { DivisionProps, PlayerProps, TeamProps } from "./Roster";
 interface LeagueGroupsProps {
   league: string;
   teams: TeamProps[];
   players: PlayerProps[];
+  divisions: DivisionProps[];
 }
 
 function LeagueGroups() {
-  const { league, teams, players }: LeagueGroupsProps = useLocation().state;
+  const { league, teams, players, divisions }: LeagueGroupsProps =
+    useLocation().state;
+  const groupLetters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  const displayGroups = () => {
+    const currentDivision = divisions.find(division => division.divisionName === league);
+    
+    if (!currentDivision) {
+      return <div>No Groups :(</div>;
+    }
+
+    return Array.from({ length: currentDivision.groups }, (_, i) => {
+      const groupLetter = groupLetters[i];
+      return (
+        <NavLink
+          key={groupLetter}
+          state={{
+            league: league,
+            teams: teams,
+            players: players,
+          }}
+          to={groupLetter.toLowerCase()}
+          className={`card cursor-pointer hover:bg-gradient-to-br from-gold-light to-gold-dark transition-all duration-1000 flex items-center justify-center w-4/5 md:w-2/3 lg:w-1/2 min-h-32 md:h-40 rounded-lg bg-gray/80 dark:bg-gray/40`}
+        >
+          <h2 className="text-3xl text-white text-center font-semibold">
+            Group {groupLetter}
+          </h2>
+        </NavLink>
+      );
+    });
+  };
+
   return (
     <div className=" relativeaccounts bg-white text-black dark:bg-black dark:text-white min-h-screen">
       <Link
@@ -25,64 +56,7 @@ function LeagueGroups() {
         </p>
         <div className="cardContainerContainer flex flex-col w-full justify-center items-center gap-8">
           <div className="cardContainer flex flex-col md:flex-row justify-center items-center gap-8 z-10 w-4/5 overflow-hidden">
-            <NavLink
-              state={{
-                group: "A",
-                league: league,
-                teams: teams,
-                players: players,
-              }}
-              to={"a"}
-              className={`card cursor-pointer hover:bg-gradient-to-br from-gold-light to-gold-dark transition-all duration-1000 flex items-center justify-center w-4/5 md:w-2/3 lg:w-1/2 min-h-32 md:h-40 rounded-lg bg-gray/80 dark:bg-gray/40`}
-            >
-              <h2 className="text-3xl text-white text-center font-semibold">
-                Group A
-              </h2>
-            </NavLink>
-            <NavLink
-              state={{
-                group: "B",
-                league: league,
-                teams: teams,
-                players: players,
-              }}
-              to={"b"}
-              className={`card cursor-pointer hover:bg-gradient-to-br from-platinum-light to-platinum-dark transition-all duration-1000 flex items-center justify-center w-4/5 md:w-2/3 lg:w-1/2 min-h-32 md:h-40 rounded-lg bg-gray/80 dark:bg-gray/40`}
-            >
-              <h2 className="text-3xl text-white text-center font-semibold">
-                Group B
-              </h2>
-            </NavLink>
-          </div>
-          <div className="cardContainer flex flex-col md:flex-row justify-center items-center gap-8 z-10 w-4/5 overflow-hidden">
-            <NavLink
-              state={{
-                group: "C",
-                league: league,
-                teams: teams,
-                players: players,
-              }}
-              to={"c"}
-              className={`card cursor-pointer hover:bg-gradient-to-br from-emerald-light to-emerald-dark transition-all duration-1000 flex items-center justify-center w-4/5 md:w-2/3 lg:w-1/2 min-h-32 md:h-40 rounded-lg bg-gray/80 dark:bg-gray/40`}
-            >
-              <h2 className="text-3xl text-white text-center font-semibold">
-                Group C
-              </h2>
-            </NavLink>
-            <NavLink
-              state={{
-                group: "D",
-                league: league,
-                teams: teams,
-                players: players,
-              }}
-              to={"d"}
-              className={`card cursor-pointer hover:bg-gradient-to-br from-challenger-blue to-challenger-gold transition-all duration-1000 flex items-center justify-center w-4/5 md:w-2/3 lg:w-1/2 min-h-32 md:h-40 rounded-lg bg-gray/80 dark:bg-gray/40`}
-            >
-              <h2 className="text-3xl text-white text-center font-semibold">
-                Group D
-              </h2>
-            </NavLink>
+            {displayGroups()}
           </div>
         </div>
         {/* <div className="teamContainer flex flex-col gap-8 md:w-3/5 lg:w-3/6 overflow-hidden justify-center">
