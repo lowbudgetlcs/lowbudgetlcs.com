fb05a7eedc8169bafae47a786b75eed8bb45a8d9
Mamii868 <marcmotenii@gmail.com>
2024-08-28 11:46:33 -0500

    Change rate limit

--- server/server.ts
+++ server/server.ts
@@ -15,8 +15,8 @@ if (!clientID || !clientSecret) {
 }
 //Rate limiting
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // Limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 500, // Limit each IP to 500 requests per windowMs
 });
 app.use("/api/", apiLimiter);
 
