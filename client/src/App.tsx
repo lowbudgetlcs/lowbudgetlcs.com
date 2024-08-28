3bb18f69bb5bb675bc3d340e6237bdf527a7456c
Mamii868 <marcmotenii@gmail.com>
2024-08-28 13:25:01 -0500

    Move routes around

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import ScrollToTop from "./components/ScrollToTop";
-//! Enable Twitch on prod
 import Twitch from "./components/Twitch";
 import Roster from "./components/Roster";
 import LeagueGroups from "./components/LeagueGroups";
@@ -16,46 +14,20 @@ import { LeagueDataProvider } from "./components/leagueDataContext";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <Twitch/>
+      <Navbar />
       <LeagueDataProvider>
-        <ScrollToTop />
-        <Navbar />
-        <Twitch />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="*" element={<ErrorPage />} />
           <Route path="rosters" element={<Roster />} />
-
-          {/* Economy League */}
-          <Route path="rosters/economy" element={<LeagueGroups />} />
-          <Route path="rosters/economy/a" element={<LeaguePlayers />} />
-          <Route path="rosters/economy/b" element={<LeaguePlayers />} />
-          <Route path="rosters/economy/c" element={<LeaguePlayers />} />
-          <Route path="rosters/economy/d" element={<LeaguePlayers />} />
-
-          {/* Commercial League */}
-          <Route path="rosters/commercial" element={<LeagueGroups />} />
-          <Route path="rosters/commercial/a" element={<LeaguePlayers />} />
-          <Route path="rosters/commercial/b" element={<LeaguePlayers />} />
-          <Route path="rosters/commercial/c" element={<LeaguePlayers />} />
-          <Route path="rosters/commercial/d" element={<LeaguePlayers />} />
-
-          {/* Financial League */}
-          <Route path="rosters/financial" element={<LeagueGroups />} />
-          <Route path="rosters/financial/a" element={<LeaguePlayers />} />
-          <Route path="rosters/financial/b" element={<LeaguePlayers />} />
-          <Route path="rosters/financial/c" element={<LeaguePlayers />} />
-          <Route path="rosters/financial/d" element={<LeaguePlayers />} />
-
-          {/* Executive League */}
-          <Route path="rosters/executive" element={<LeagueGroups />} />
-          <Route path="rosters/executive/a" element={<LeaguePlayers />} />
-          <Route path="rosters/executive/b" element={<LeaguePlayers />} />
-          <Route path="rosters/executive/c" element={<LeaguePlayers />} />
-          <Route path="rosters/executive/d" element={<LeaguePlayers />} />
+          <Route path="rosters/:league" element={<LeagueGroups />} />
+          <Route path="rosters/:league/:group" element={<LeaguePlayers />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
-        <Footer />
       </LeagueDataProvider>
+      <Footer />
     </BrowserRouter>
   );
 }
