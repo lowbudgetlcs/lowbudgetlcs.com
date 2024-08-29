07b65a5909b0ab6fb47cb68cfb5fd4808387f336
Mamii868 <marcmotenii@gmail.com>
2024-08-29 07:04:39 -0500

    Edit env imports

--- client/src/leagueData.ts
+++ client/src/leagueData.ts
@@ -38,9 +38,9 @@ export const useFetchData = () => {
     const fetchData = async () => {
       try {
         const [playersResponse, teamsResponse, divisionsResponse] = await Promise.all([
-          fetch(import.meta.env.VITE_PLAYERS_URL as string),
-          fetch(import.meta.env.VITE_TEAMS_URL as string),
-          fetch(import.meta.env.VITE_DIVISIONS_URL as string),
+          fetch(process.env.VITE_PLAYERS_URL as string),
+          fetch(process.env.VITE_TEAMS_URL as string),
+          fetch(process.env.VITE_DIVISIONS_URL as string),
         ]);
 
         if (!playersResponse.ok || !teamsResponse.ok || !divisionsResponse.ok) {
