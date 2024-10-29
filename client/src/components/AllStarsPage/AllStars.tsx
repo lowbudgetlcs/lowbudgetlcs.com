fa1d93e34e905aa9887699597413d2a21a63956c
Mamii868 <marcmotenii@gmail.com>
2024-10-29 08:22:30 -0500

    Edit css for text

--- client/src/components/AllStarsPage/AllStars.tsx
+++ client/src/components/AllStarsPage/AllStars.tsx
@@ -5,7 +5,7 @@ function AllStars() {
         <h1 className="text-6xl">All Stars</h1>
       </div>
       <div className="flex flex-col items-center pb-8">
-        <p className="summary text-lg md:text-xl px-16 w-full md:w-2/3 pt-8 text-center">
+        <p className="summary text-lg md:text-xl px-16 w-full md:w-2/3 pt-8  ">
           After Every LBLCS season, each team votes for players on who is the
           best in their role. We then take those results and smash all these
           stars into teams of their own and make them compete for charity!
@@ -14,13 +14,13 @@ function AllStars() {
           All proceeds gained from the stream go to the charity announced in
           discord. Please donate to help the cause!
         </p>
-        <p className="text-white/60 text-lg pb-8">
+        <p className="text-white/60 text-lg">
           The 1st all-star teams play in the event. If people are not available,
           2nd and 3rd team members are asked to fill in.
         </p>
       </div>
       <div className="cardContainer">
-        <div className="firstTeams"></div>
+        <div className="firstTeams text-3xl font-bold text-orange text-center py-32">Teams Announced Soon</div>
       </div>
     </div>
   );
