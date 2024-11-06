3e74744324fa41d8005cebdfcc4b806d7f14f1e7
Mamii868 <marcmotenii@gmail.com>
2024-11-06 16:59:45 -0600

    Add placeholder image

--- client/src/components/AllStarsPage/ASFinancial.tsx
+++ client/src/components/AllStarsPage/ASFinancial.tsx
@@ -1,6 +1,9 @@
+import testImage from "../../assets/FinaB3.png"
+
 function ASFinancial() {
-    return(<div>
-        <img src="https://imgur.com/MrtaWTd"></img>
+    return(<div className="flex flex-col gap-4 items-center p-4">
+       <h2 className="text-xl font-bold">Financial Group A</h2>
+       <img className="md:w-1/2" src={testImage}></img>
     </div>)
 }
 
