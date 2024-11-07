a92c25342ce7156c0a23530e0b6f4a0d232fda5a
Mamii868 <marcmotenii@gmail.com>
2024-11-06 19:05:39 -0600

    Add imgur links for images

--- client/src/components/AllStarsPage/ASFinancial.tsx
+++ client/src/components/AllStarsPage/ASFinancial.tsx
@@ -3,14 +3,24 @@ function ASFinancial() {
     const color = 'bg-gradient-to-r from-emerald-light to-emerald-dark'
     return(<div className="flex flex-col gap-4 items-center p-2 md:p-4">
         <h2 className="relative text-xl font-bold">Financial Group A <span className={`absolute -bottom-0.5 left-0 w-full h-1 rounded-md ${color}`}></span></h2>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/jd3vWZQ' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/lvZoaxo' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/hOiRaTg' alt="all star team with player names"></img>
 
        <h2 className="relative text-xl font-bold">Financial Group B <span className={`absolute -bottom-0.5 left-0 w-full h-1 rounded-md ${color}`}></span></h2>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
-       <img className="md:w-3/4 lg:w-2/3" src='' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/IkWaROS' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/zt0256m' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/HNtocAj' alt="all star team with player names"></img>
+
+       <h2 className="relative text-xl font-bold">Financial Group C <span className={`absolute -bottom-0.5 left-0 w-full h-1 rounded-md ${color}`}></span></h2>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/aA7V7ri' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/Cngbb1O' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/bLYtsiz' alt="all star team with player names"></img>
+       
+       <h2 className="relative text-xl font-bold">Financial Group D <span className={`absolute -bottom-0.5 left-0 w-full h-1 rounded-md ${color}`}></span></h2>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/sDsycgC' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/F4cxKyG' alt="all star team with player names"></img>
+       <img className="md:w-3/4 lg:w-2/3" src='https://imgur.com/gykVnxT' alt="all star team with player names"></img>
     </div>)
 }
 
