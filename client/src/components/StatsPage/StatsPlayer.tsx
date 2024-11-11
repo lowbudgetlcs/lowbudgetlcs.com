2d4f2845511857c932c0309a7607fb89b4e7dc4e
Mamii868 <marcmotenii@gmail.com>
2024-11-11 13:20:09 -0600

    Add more data display and correct time

--- client/src/components/StatsPage/StatsPlayer.tsx
+++ client/src/components/StatsPage/StatsPlayer.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-
-
 interface Game {
   id: number;
   kills: number;
@@ -39,26 +37,23 @@ interface LocationState {
 function StatsPlayer() {
   const navigate = useNavigate();
   const location = useLocation();
-
   const state = location.state as LocationState | undefined;
 
   const [champImages, setChampImages] = useState<Record<string, string>>({});
 
   useEffect(() => {
-    // Use import.meta.glob to dynamically import all champion images
     const modules = import.meta.glob("../../assets/champion/*.png");
     const images: Record<string, string> = {};
 
-    // Load each image and populate the images object
     const loadImages = async () => {
       for (const path in modules) {
-        const name = path.match(/([^/]+)(?=\.\w+$)/)?.[0]; // Extract the filename without extension
+        const name = path.match(/([^/]+)(?=\.\w+$)/)?.[0];
         if (name) {
-            const module = (await modules[path]()) as { default: string };
+          const module = (await modules[path]()) as { default: string };
           images[name] = module.default;
         }
       }
-      setChampImages(images); // Update state once all images are loaded
+      setChampImages(images);
     };
 
     loadImages();
@@ -73,8 +68,16 @@ function StatsPlayer() {
   const allGameData: Game[] = state.gameData.flat();
   const summonerName = state.summonerName;
 
-  console.log(allGameData);
-
+  const handleTime = (gameLength: number) => {
+    const minutes = Math.floor(gameLength / 60);
+    let seconds = gameLength - minutes * 60;
+    let stringSeconds = seconds.toString()
+    if (seconds === 0) {
+        stringSeconds = '00'
+    }
+    const time = minutes + ':' + stringSeconds
+    return time
+}
   return (
     <div className="aboutbg-white text-black dark:bg-black dark:text-white font-serif">
       <div className="title min-h-64 mb-16 mt-40 sm:mt-24 w-full flex flex-col md:flex-row items-center justify-center gap-8 md:gap-16">
@@ -85,63 +88,43 @@ function StatsPlayer() {
       </div>
       <div className="flex flex-col items-center py-8 px-4 gap-8">
         {allGameData.map((game) => {
-            const champImageSrc = champImages[game.championName] || 'default.png';
+          const champImageSrc = champImages[game.championName] || 'default.png';
           return (
-            <div key={game.id} className="gameContainer w-full min-h-44 md:h-44 py-8 bg-gray flex flex-col md:flex-row rounded-md">
-              <div className="gameInfo flex flex-col md:px-4 items-center justify-center gap-2 py-2">
+            <div key={game.id} className="gameContainer w-full md:h-44 py-8 bg-gray flex flex-col md:flex-row rounded-md">
+              <div className="gameInfo flex flex-col items-center justify-center gap-2 py-2 w-full md:w-24 px-4 text-left">
                 <h2>{game.win ? "Win" : "Loss"}</h2>
                 <div className="line h-1 w-16 rounded-md bg-orange"></div>
-                <h2>gameLength</h2>
+                <h2>{handleTime(game.gameLength)}</h2>
               </div>
-              <div className="champKDA flex flex-col gap-2 py-2 md:px-4 items-center justify-center m-4">
+              <div className="champKDA flex flex-col gap-2 py-2 items-center justify-center w-full md:w-48 text-left">
                 <img
                   src={champImageSrc}
+                  alt={game.championName}
                   className="champion min-w-[50px] min-h-[50px] max-w-[50px] max-h-[50px] bg-dark-blue"
                 />
-                <p className="text-xl font-semibold">k/d/a</p>
+                <p className="text-xl font-semibold">{game.kills}/{game.deaths}/{game.assists}</p>
               </div>
-              <div className="fullStats flex flex-col md:flex-row gap-4">
-                <div className="goldStats flex flex-col gap-2 items-center md:items-start justify-center">
-                  <p className="text-base">
-                    CS: <span className="text-white/60">400</span>
-                  </p>
-                  <p className="text-base">
-                    CS/M: <span className="text-white/60">12</span>
-                  </p>
-                  <p className="text-base">
-                    Gold: <span className="text-white/60">300000</span>
-                  </p>
-                  <p className="text-base">
-                    Vision Score: <span className="text-white/60">200</span>
-                  </p>
-                </div>
-                <div className="damageStats flex flex-col gap-2 items-center md:items-start justify-center">
-                  <p className="text-base">
-                    Damage: <span className="text-white/60">5023443</span>
-                  </p>
-                  <p className="text-base">
-                    Healing: <span className="text-white/60">5023512</span>
-                  </p>
-                  <p className="text-base">
-                    Shielding: <span className="text-white/60">502343</span>
-                  </p>
-                  <p className="text-base">
-                    Damage to Turrets:{" "}
-                    <span className="text-white/60">201442</span>
-                  </p>
-                </div>
-                <div className="defenceStats flex flex-col gap-2 items-center md:items-start justify-center">
-                  <p className="text-base">
-                    Damage Taken:{" "}
-                    <span className="text-white/60">23424642</span>
-                  </p>
-                  <p className="text-base">
-                    Self-Mitigated:{" "}
-                    <span className="text-white/60">3453452</span>
-                  </p>
+              {/* Container to align stats consistently across all screen sizes */}
+              <div className="fullStats flex flex-col md:flex-row gap-4 items-center justify-center w-fit md:w-1/2 text-left m-auto md:m-4 pt-4 flex-grow">
+                <div className="statsContainer flex flex-col gap-2 md:flex-row w-full items-center">
+                  <div className="goldStats flex flex-col gap-2 items-start justify-center w-full md:w-1/3">
+                    <p className="text-sm">CS: <span className="text-white/60">{game.cs}</span></p>
+                    <p className="text-sm">CS/m: <span className="text-white/60">{(game.cs / 60).toFixed(1)}</span></p>
+                    <p className="text-sm">Gold: <span className="text-white/60">{game.gold}</span></p>
+                    <p className="text-sm">Vision Score: <span className="text-white/60">{game.visionScore}</span></p>
+                  </div>
+                  <div className="damageStats flex flex-col gap-2 items-start justify-center w-full md:w-1/3">
+                    <p className="text-sm">Damage: <span className="text-white/60">{game.damage}</span></p>
+                    <p className="text-sm">Healing: <span className="text-white/60">{game.healing}</span></p>
+                    <p className="text-sm">Shielding: <span className="text-white/60">{game.shielding}</span></p>
+                  </div>
+                  <div className="defenceStats flex flex-col gap-2 items-start justify-center w-full md:w-1/3">
+                    <p className="text-sm">Damage Taken: <span className="text-white/60">{game.damageTaken}</span></p>
+                    <p className="text-sm">Self-Mitigated: <span className="text-white/60">{game.selfMitigatedDamage}</span></p>
+                  </div>
                 </div>
               </div>
-            </div>
+              </div>
           );
         })}
       </div>
