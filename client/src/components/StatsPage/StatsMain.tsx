2d7748d5331117986d058ae1614c687399f8f803
Mamii868 <marcmotenii@gmail.com>
2024-11-28 17:31:13 -0600

    Remove spacing

--- client/src/components/StatsPage/StatsMain.tsx
+++ client/src/components/StatsPage/StatsMain.tsx
@@ -11,7 +11,6 @@ function StatsMain() {
           onClick={() => setIsSearchActive(false)}
           className="fixed flex z-50 my-2 px-2 rounded-lg top-1 left-16 text-2xl font-semibold cursor-pointer w-fit h-fit justify-center items-center  group"
         >
-          {" "}
           <div className="burger cursor-pointer relative h-12 w-6 gap-1 hover:cursor-pointer self-baseline">
             <div
               className={`absolute -rotate-45 top-5 left-0 transition-all duration-300 px-2 py-0.5 rounded-xl bg-white group-hover:bg-orange`}
@@ -56,8 +55,3 @@ function StatsMain() {
 }
 
 export default StatsMain;
-
-// <div className="statsSeasonList flex flex-col flex-grow">
-// <h2 className="text-center text-xl font-bold">Choose a Season</h2>
-// <Button>2023</Button>
-// </div>
