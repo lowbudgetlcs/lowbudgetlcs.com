195fe9aac07be2ddbfd143eef5eaaa2f240a3d37
Mamii868 <marcmotenii@gmail.com>
2024-12-01 13:49:48 -0600

    Remove back button
    Not needed after StatsMain render changes

--- client/src/components/StatsPage/StatsTeam.tsx
+++ client/src/components/StatsPage/StatsTeam.tsx
@@ -3,27 +3,15 @@ import Button from "../Button";
 
 function StatsTeam() {
   return (
-    <div className="seasonList flex flex-col flex-grow">
-      <Link
-        to={`/stats`}
-        className="fixed flex z-50 my-2 px-2 rounded-lg top-1 left-16 text-2xl font-semibold cursor-pointer w-fit h-fit justify-center items-center  group"
-      >
-        <div className="burger cursor-pointer relative h-12 w-6 gap-1 hover:cursor-pointer self-baseline">
-          <div
-            className={`absolute -rotate-45 top-5 left-0 transition-all duration-300 px-2 py-0.5 rounded-xl bg-white group-hover:bg-orange`}
-          ></div>
-          <div
-            className={`absolute rotate-45 top-7 left-0 transition-all duration-300 px-2 py-0.5 rounded-xl bg-white group-hover:bg-orange`}
-          ></div>
-        </div>
-        <p className="group-hover:text-orange underline transition duration-300">
-          Back
-        </p>
+    <div className="seasonList flex flex-col justify-center items-center py-4 text-center">
+      <h2 className="text-center text-2xl font-bold p-4">Choose a Season</h2>
+      <Link to={"s13"}>
+        <Button>
+          Season 13
+          <br />
+          <span>Summer/Fall 2024</span>
+        </Button>
       </Link>
-      <h2 className="text-center text-xl font-bold">Choose a Season</h2>
-      <Button>
-        Season 13 <br /> <span>Summer/Fall 2023</span>
-      </Button>
     </div>
   );
 }
