070721e6a17524203353bd1ad75c9c391c81d463
Mamii868 <marcmotenii@gmail.com>
2024-12-01 13:46:35 -0600

    Refactor buttons to allow correct display
    This will let both the search bar and the teams choices render on the same page when their respective button is clicked. state variables are used to control what is rendred. The back button sets both states to false when clicked.

--- client/src/components/StatsPage/StatsMain.tsx
+++ client/src/components/StatsPage/StatsMain.tsx
@@ -1,14 +1,47 @@
 import { useState } from "react";
 import StatsSearchUI from "./StatsSearchUI";
+import StatsTeam from "./StatsTeam";
 
 function StatsMain() {
   const [isSearchActive, setIsSearchActive] = useState<Boolean>(false);
+  const [isTeamsActive, setIsTeamsActive] = useState<Boolean>(false);
 
+  const displayStatOptions = () => {
+    if (isSearchActive) {
+      return <StatsSearchUI />;
+    } else if (isTeamsActive) {
+      return <StatsTeam />;
+    } else {
+      return (
+        <div className="statsMainContainer flex flex-col md:flex-row justify-center p-8 gap-8">
+          <div
+            onClick={() => setIsSearchActive(true)}
+            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
+          >
+            <h2 className="text-3xl text-white text-center font-semibold z-10">
+              Search Player
+            </h2>
+          </div>
+          <div
+            onClick={() => setIsTeamsActive(true)}
+            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
+          >
+            <h2 className="text-3xl text-white text-center font-semibold z-10">
+              Team Stats
+            </h2>
+          </div>
+        </div>
+      );
+    }
+  };
   return (
     <div className="aboutbg-white text-black dark:bg-black dark:text-white font-serif">
-      {isSearchActive ? (
+      {isSearchActive || isTeamsActive ? (
         <div
-          onClick={() => setIsSearchActive(false)}
+          onClick={() => {
+            setIsSearchActive(false);
+            setIsTeamsActive(false);
+          }}
           className="fixed flex z-50 my-2 px-2 rounded-lg top-1 left-16 text-2xl font-semibold cursor-pointer w-fit h-fit justify-center items-center  group"
         >
           <div className="burger cursor-pointer relative h-12 w-6 gap-1 hover:cursor-pointer self-baseline">
@@ -29,27 +62,7 @@ function StatsMain() {
       <div className="title h-64 w-full flex items-center justify-center">
         <h1 className="text-6xl text-center">LBLCS Stats</h1>
       </div>
-      {isSearchActive ? (
-        <StatsSearchUI />
-      ) : (
-        <div className="statsMainContainer flex flex-col md:flex-row justify-center p-8 gap-8">
-          <div
-            onClick={() => setIsSearchActive(true)}
-            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
-          >
-            <h2 className="text-3xl text-white text-center font-semibold z-10">
-              Search Player
-            </h2>
-          </div>
-          <div
-            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
-          >
-            <h2 className="text-3xl text-white text-center font-semibold z-10">
-              Team Stats
-            </h2>
-          </div>
-        </div>
-      )}
+      {displayStatOptions()}
     </div>
   );
 }
