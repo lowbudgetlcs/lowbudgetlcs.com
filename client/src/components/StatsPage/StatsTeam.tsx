4fd549e5e717f63f9a5f3300abde114eb335e5ee
Mamii868 <marcmotenii@gmail.com>
2024-12-18 19:36:53 -0600

    Add selection for team details

--- client/src/components/StatsPage/StatsTeam.tsx
+++ client/src/components/StatsPage/StatsTeam.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 interface StatsProps {
   teamName: string;
@@ -12,7 +13,13 @@ interface StatsProps {
 
 function StatsTeam() {
       const { teamName, groupId, divisionId, logo, playerList }: StatsProps = useLocation().state;
+      const [activeLink, setActiveLink] = useState<string>("Details");
+
+      const toggleActive = (navItem: string) => {
+        setActiveLink(navItem);
+      };
   return (
+    <>
     <div className="statsTeamTitle min-h-64 mb-16 mt-40 sm:mt-24 w-full flex flex-col md:flex-row items-center justify-center gap-8 md:gap-16">
       {logo ? (
         <img
@@ -25,6 +32,34 @@ function StatsTeam() {
 
       <h1 className="text-6xl text-center text-white">{teamName}</h1>
     </div>
+    <div className="navList text-white">
+            <ul className="relative flex gap-4 text-sm sm:text-base md:text-2xl font-semibold justify-center p-4">
+              <li
+                onClick={() => toggleActive("Details")}
+                className="relative active:text-orange hover:text-orange transition duration-300 cursor-pointer"
+              >
+                Details
+                <span
+                  className={`line absolute ${
+                    activeLink === "Details" ? "w-full" : "w-0"
+                  } h-0 transition-all duration-200 border-b-4 border-orange rounded-md bg-orange -bottom-0.5 right-0`}
+                ></span>
+              </li>
+              <li
+                onClick={() => toggleActive("History")}
+                className="relative active:text-orange hover:text-orange transition duration-300 cursor-pointer"
+              >
+                Match History
+                <span
+                  className={`line absolute ${
+                    activeLink === "History" ? "w-full" : "w-0"
+                  } h-0 transition-all duration-200 border-b-4 border-orange rounded-md bg-orange -bottom-0.5 left-0`}
+                ></span>
+              </li>
+            </ul>
+          </div>
+          <div className="detailsSection"></div>
+    </>
   );
 }
 
