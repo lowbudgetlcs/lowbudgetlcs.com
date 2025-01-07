227c554ab1f20c81c46366a78c8708c62c66e851
Mamii868 <marcmotenii@gmail.com>
2025-01-07 15:41:32 -0600

    Create basic UI for draft creation

--- client/src/components/DraftTool/DraftMain.tsx
+++ client/src/components/DraftTool/DraftMain.tsx
@@ -1,9 +1,38 @@
+import Button from "../Button";
+
 function DraftMain() {
   return (
-    <>
-      <div>Draft Tool</div>
-      <div>Initial Upload</div>
-    </>
+    <div className="text-white">
+      <div className="title m-20 text-center">
+        <h1 className="text-6xl text-white">Draft Tool</h1>
+      </div>
+      <div className="draftOptions">
+        <h2 className="text-center text-2xl font-bold">Create Draft</h2>
+        <form className="flex flex-col items-center gap-4 justify-center p-4">
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="flex flex-col">
+              <p>Blue Team</p>
+              <input
+                type="text"
+                placeholder="Blue Team"
+                className="bg-gray/40 border-gray border-2 rounded-md p-2 text-blue"
+              ></input>
+            </div>
+            <div className="flex flex-col">
+              <p>Red Team</p>
+              <input
+                type="text"
+                placeholder="Red Team"
+                className="bg-gray/40 border-gray border-2 rounded-md p-2 text-red"
+              ></input>
+            </div>
+          </div>
+          <button type="submit" className="">
+            <Button>Create Draft</Button>
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
