eb8e42901858fd57613e8df119aa76f86f35e304
Mamii868 <marcmotenii@gmail.com>
2025-01-07 15:08:22 -0600

    Fix fetch urls
    forgot to add backend

--- client/src/leagueData.ts
+++ client/src/leagueData.ts
@@ -38,9 +38,9 @@ export const useFetchData = () => {
     const fetchData = async () => {
       try {
         const [playersResponse, teamsResponse, divisionsResponse] = await Promise.all([
-          fetch("https://lowbudgetlcs.com/api/getPlayers"),
-          fetch("https://lowbudgetlcs.com/api/getTeams"),
-          fetch("https://lowbudgetlcs.com/api/getDivisions"),
+          fetch("https://backend.lowbudgetlcs.com/api/getPlayers"),
+          fetch("https://backend.lowbudgetlcs.com/api/getTeams"),
+          fetch("https://backend.lowbudgetlcs.com/api/getDivisions"),
         ]);
 
         if (!playersResponse.ok || !teamsResponse.ok || !divisionsResponse.ok) {
