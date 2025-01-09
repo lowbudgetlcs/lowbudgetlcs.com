3c886fef0ed26c09a8020460aa1a3b1e6e0bdbf4
Mamii868 <marcmotenii@gmail.com>
2025-01-09 00:30:34 -0600

    Add insert function for draft lobby

---
+++ server/db/queries/insert.ts
@@ -0,0 +1,24 @@
+import { db } from "../index";
+import { DraftProps } from "../../server";
+import { draftLobbies } from "../schema";
+
+export async function insertDraft(draft: DraftProps) {
+  try {
+    const insertDraft = await db
+      .insert(draftLobbies)
+      .values({
+        lobbycode: draft.lobbyCode,
+        bluecode: draft.blueCode,
+        redcode: draft.redCode,
+        shortcode: draft.tournamentID,
+        bluename: draft.blueName,
+        redname: draft.redName,
+      })
+      .returning();
+
+      console.log("Draft inserted successfully:", insertDraft);
+  } catch (err) {
+    console.error("Error inserting into DB: ", err);
+    throw new Error("Failed to insert draft into database.");
+  }
+}
