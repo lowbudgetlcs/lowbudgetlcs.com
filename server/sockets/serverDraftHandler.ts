01b9bc8e3ed619566f4c68efe0aec254c167a3f7
Mamii868 <marcmotenii@gmail.com>
2025-01-14 22:13:13 -0600

    Attempt to fix ban phase issues

--- server/sockets/serverDraftHandler.ts
+++ server/sockets/serverDraftHandler.ts
@@ -8,17 +8,15 @@ export const draftHandler = async (
   redUser: string
 ) => {
   let banPhaseStart = false;
-  let banPhase1: number = 6;
-  let banPhase2: number = 4;
+  const bansPhase1 = [blueUser, redUser, redUser, blueUser, blueUser, redUser];
+  const bansPhase2 = [redUser, blueUser, blueUser, redUser];
+  const picksPhase1 = [blueUser, redUser, redUser, blueUser, blueUser, redUser];
+  const picksPhase2 = [redUser, blueUser, blueUser, redUser];
   const bansArray: Array<string> = [];
-  console.log("Ban Phase Starting");
-  //   Tells client ban phase has begun then runs first ban phase
-  io.to(lobbyCode).emit("banPhase", true);
-  banPhaseStart = true;
   // Runs ban phase while total bans is less than 6
   const handleTurn = async (currentSide: string) => {
     let timer = 34;
-    return new Promise((resolve: any) => {
+    return new Promise<void>((resolve) => {
       let interval = setInterval(() => {
         timer--;
         console.log(timer);
@@ -31,25 +29,23 @@ export const draftHandler = async (
         }
       }, 1000);
 
-      socket.once(
-        "ban",
-        ({
-          sideCode,
-          chosenChamp,
-        }: {
-          sideCode: string;
-          chosenChamp: string;
-        }) => {
-          console.log("Ban Recieved: ", chosenChamp);
-          if (sideCode === currentSide) {
-            clearInterval(interval);
-            bansArray.push(chosenChamp);
-            console.log("Ban has been given: ", chosenChamp);
-            io.to(lobbyCode).emit("setBan", { chosenChamp });
-            resolve();
-          }
+      const banListener = ({
+        sideCode,
+        chosenChamp,
+      }: {
+        sideCode: string;
+        chosenChamp: string;
+      }) => {
+        console.log("Ban Recieved: ", chosenChamp);
+        if (sideCode === currentSide) {
+          clearInterval(interval);
+          bansArray.push(chosenChamp);
+          console.log("Ban has been given: ", chosenChamp);
+          io.to(lobbyCode).emit("setBan", { chosenChamp });
+          resolve();
         }
-      );
+      };
+      socket.on("ban", banListener);
 
       io.to(lobbyCode).emit("banTurn", currentSide);
     });
@@ -57,17 +53,17 @@ export const draftHandler = async (
 
   // Blue side's turn
   const startBanPhase = async () => {
-    for (let i = 0; i < banPhase1; i++) {
-      const isBlueTurn = i % 2 === 0;
-      //   Use this to track current side making the turn
-      const currentSide = isBlueTurn ? blueUser : redUser;
+    for (const currentSide of bansPhase1) {
       console.log("currentTurn: ", currentSide);
       io.to(lobbyCode).emit("currentTurn", currentSide);
       await handleTurn(currentSide);
       console.log("switching turns");
     }
   };
-
+  console.log("Ban Phase Starting");
+  // Tells client ban phase has begun then runs first ban phase
+  io.to(lobbyCode).emit("banPhase", true);
+  banPhaseStart = true;
   await startBanPhase();
   console.log("Ban Phase 1 is complete :)");
 };
