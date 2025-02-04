123e5eb03cadcf7806a18355aee050f9283fd8a8
Mamii868 <marcmotenii@gmail.com>
2025-02-03 23:15:10 -0600

    Remove unused image code
    Moving to the ccdn lets me lessen the amount of unique links needed for champions

--- client/src/components/DraftTool/LoadChampLargeImages.tsx
+++ client/src/components/DraftTool/LoadChampLargeImages.tsx
@@ -1,7 +1,7 @@
 import tempImage from "../../assets/Transparent_LBLCS_Logo.png";
 
 const dDragonLargeImageLink =
-  "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/";
+  `https://cdn.communitydragon.org/15.2.1/champion/`;
 
 const cDragonCDNSplashLink = `https://cdn.communitydragon.org/15.2.1/champion/`;
 
@@ -12,52 +12,28 @@ export const DisplayBanImage = (
   const championName = bannedChampions[banIndex]
     ? bannedChampions[banIndex].toLowerCase()
     : "nothing";
-  const correctChampionImageLinks = () => {
-    let link = `${dDragonLargeImageLink}${championName}/skins/base/${championName}loadscreen.jpg`;
-    switch (championName) {
-      case "naafiri":
-      case "milio":
-        link = `${dDragonLargeImageLink}${championName}/skins/base/${championName}loadscreen_0.jpg`;
-        break;
-      case "ambessa":
-        link = `${dDragonLargeImageLink}${championName}/skins/base/${championName}loadscreen_0.domina.jpg`;
-        break;
-      case "hwei":
-        link = `${dDragonLargeImageLink}${championName}/skins/skin0/${championName}loadscreen_0.jpg`;
-        break;
-      case "aurora":
-      case "mel":
-        link = `${dDragonLargeImageLink}${championName}/skins/base/${championName}loadscreen_0.${championName}.jpg`;
-        break;
-    }
-
-    return link;
-  };
+    const championLink = `${dDragonLargeImageLink}${championName}/splash-art/centered`;
+  console.log("championLink: ", championLink)
   if (bannedChampions[banIndex] === "nothing") {
     return (
       <img
         src={tempImage}
         alt={`nothing`}
-        style={{
-          width: "160px",
-          height: "200px",
-          objectFit: "cover",
-        }}
+        width={"160px"}
+        height={"200px"}
+        className="object-cover"
       />
     );
   } else {
     return (
       <>
         {bannedChampions[banIndex] && (
-          <img
-            src={correctChampionImageLinks()} //largeChampImages[bannedChampions[banIndex]]
-            alt={`${bannedChampions[banIndex]}`}
-            style={{
-              width: "160px",
-              height: "200px",
-              objectFit: "cover",
-            }}
-          />
+          <div
+          style={{
+            backgroundImage: `url('https://cdn.communitydragon.org/15.2.1/champion/${championName}/splash-art/centered')`
+          }}
+            className={`bg-cover h-20 w-20 z-10  bg-[url('https://cdn.communitydragon.org/15.2.1/champion/${championName}/splash-art/centered')]`}
+          ></div>
         )}
       </>
     );
@@ -73,22 +49,6 @@ export const DisplayPickImage = (
 
   const correctChampionImageLinks = () => {
     let link = `${cDragonCDNSplashLink}${championName}/splash-art/centered`;
-    switch (championName) {
-      case "naafiri":
-      case "milio":
-        link = `${cDragonCDNSplashLink}${championName}/splash-art/centered`;
-        break;
-      case "ambessa":
-        link = `${cDragonCDNSplashLink}${championName}/splash-art/centered`;
-        break;
-      case "hwei":
-        link = `${cDragonCDNSplashLink}${championName}/splash-art/centered`;
-        break;
-      case "aurora":
-      case "mel":
-        link = `${cDragonCDNSplashLink}${championName}/splash-art/centered`;
-        break;
-    }
     return link;
   };
 
@@ -109,7 +69,7 @@ export const DisplayPickImage = (
           <img
             src={correctChampionImageLinks()} //largeChampImages[pickedChampions[pickIndex]]
             alt={`${pickedChampions[pickIndex]}`}
-            className="max-w-full max-h-full object-cover"
+            className="max-w-full max-h-full object-cover scale-150"
             width={"300px"}
             height={"90px"}
           />
