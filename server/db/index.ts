f8c87bba1fd44c2b77789e5b8e737ab005e63f78
ruuffian <ruuffian@gmail.com>
2025-02-10 17:24:16 -0500

    I misunderstood the bang operator, re-added null check and throw.

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+if (!process.env.DATABASE_URL) throw Error("DATABASE_URL not set.");
+const client = postgres(process.env.DATABASE_URL, { prepare: false });
 export const db = drizzle(client);
 
 // async function testConnection() {
