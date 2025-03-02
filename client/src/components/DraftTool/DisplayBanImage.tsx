77be122c887fadaf5cd5a7f6eefa75f23787b1ef
Mamii868 <marcmotenii@gmail.com>
2025-03-02 15:04:58 -0600

    Fix images reloading

--- client/src/components/DraftTool/DisplayBanImage.tsx
+++ client/src/components/DraftTool/DisplayBanImage.tsx
@@ -1,4 +1,4 @@
-import { memo, useLayoutEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import tempImage from "../../assets/lblcsLogo.svg";
 import { DraftProps } from "./draftInterfaces";
 
@@ -11,59 +11,59 @@ const DisplayBanImage = ({
   bannedChampions: string[];
   currentHover: DraftProps["currentHover"];
 }) => {
-  const [mounted, setMounted] = useState<boolean>(false);
-  const delay: number = 20;
-
-  useLayoutEffect(() => {
-    setTimeout(() => setMounted(true), delay);
-  });
-
-  // Check if the current slot should show the hovered champion
-  const isChampHovered = bannedChampions.length === banIndex && currentHover;
+  const [link, setLink] = useState<string>("");
 
   const championName = bannedChampions[banIndex]
     ? bannedChampions[banIndex].toLowerCase()
     : "nothing";
 
-  if (isChampHovered) {
-    return (
-      mounted && (
-        <div
-          style={{
-            backgroundImage: `url('https://cdn.communitydragon.org/latest/champion/${
-              currentHover === "Wukong" ? "monkeyking" : currentHover
-            }/tile')`,
-          }}
-          className={`relative w-full h-full bg-cover grayscale-[90%]`}
-        ></div>
-      )
-    );
-  }
+  // Check if the current slot should show the hovered champion
+  const isChampHovered =
+    bannedChampions.length === banIndex &&
+    currentHover &&
+    championName === "nothing";
+
+  useEffect(() => {
+    if (isChampHovered) {
+      setLink((prevLink) => {
+        const imageURL = `https://cdn.communitydragon.org/latest/champion/${currentHover}/tile`;
+        if (imageURL !== prevLink) {
+          return imageURL;
+        }
+        return prevLink;
+      });
+    } else {
+      setLink((prevLink) => {
+        const imageURL = `https://cdn.communitydragon.org/latest/champion/${championName}/tile`;
+        if (imageURL !== prevLink) {
+          return imageURL;
+        }
+        return prevLink;
+      });
+    }
+  }, [currentHover, championName]);
 
   if (bannedChampions[banIndex] === "nothing") {
     return (
-      mounted && (
-        <img
-          src={tempImage}
-          alt="nothing"
-          width="160px"
-          height="200px"
-          className="object-cover scale-90 grayscale opacity-25"
-        />
-      )
+      <img
+        src={tempImage}
+        alt="nothing"
+        width="160px"
+        height="200px"
+        className="object-cover scale-90 grayscale opacity-25"
+      />
     );
-  } else if (championName !== "nothing") {
+  } else if (championName !== "nothing" || isChampHovered) {
     return (
-      mounted && (
-        <div
-          style={{
-            backgroundImage: `url('https://cdn.communitydragon.org/latest/champion/${
-              championName === "wukong" ? "monkeyking" : championName
-            }/tile')`,
-          }}
-          className={`relative w-full h-full bg-cover`}
-        ></div>
-      )
+      <div className={`relative w-full h-full`}>
+        <img
+          src={link}
+          className={`w-full h-full object-cover ${
+            isChampHovered && "grayscale-[90%]"
+          }`}
+        />
+
+      </div>
     );
   }
 };
