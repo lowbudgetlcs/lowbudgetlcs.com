67b79313e0292827eae3a4f7a779ff4bd5294a1e
Mamii868 <marcmotenii@gmail.com>
2025-03-18 11:37:34 -0500

    Remove dupliucate function

---
+++ server/statHandlers/seriesHandler.ts
@@ -0,0 +1,41 @@
+import { getSeriesData } from "../db/queries/select";
+
+const seriesHandler = async (seriesId: number) => {
+  try {
+    const rawSeriesData = await getSeriesData(seriesId);
+
+    if (!rawSeriesData) {
+      throw new Error("Series not found");
+    }
+
+    const teams = rawSeriesData.teams;
+    const games = rawSeriesData.games;
+
+    // Gets array of game ids to search for player stats
+    const gameIds = games.map((game) => game.id);
+
+    teams.forEach((team) => {
+      const gameWins = { gameWins: 0 };
+      for (let i = 0; i < games.length; i++) {
+        if (games[i].winnerId === team.teamID) {
+          gameWins.gameWins++;
+        }
+      }
+      Object.assign(team, gameWins);
+    });
+
+    const refinedSeriesData = {
+      divisionInfo: rawSeriesData.divisionInfo,
+      seriesInfo: rawSeriesData.seriesInfo,
+      teamInfo: teams,
+      gameIds: gameIds,
+      gameData: rawSeriesData.gameData,
+    };
+    
+    return refinedSeriesData;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export default seriesHandler;
