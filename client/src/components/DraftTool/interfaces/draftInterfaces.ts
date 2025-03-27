f92e12eb8eef018b98ad928986b6830d9ddb28cf
Mamii868 <marcmotenii@gmail.com>
2025-03-27 17:33:19 -0500

    Add interface file

---
+++ client/src/components/DraftTool/interfaces/draftInterfaces.ts
@@ -0,0 +1,18 @@
+export interface FearlessInitializerProps {
+    fearlessCode: string;
+    blueCode: string;
+    redCode: string;
+    blueDisplayName: string;
+    redDisplayName: string;
+    draftCount: number;
+    draftLobbyCodes: string[];
+  }
+
+export interface FearlessDraftLinkProps {
+    fearlessCode: string;
+    blueCode: string;
+    redCode: string;
+    blueName: string;
+    redName: string;
+    draftLobbyCodes: string[];
+}
