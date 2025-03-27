c9e3d0515c6ff87ea74db8cce3f8564961cb41f5
Mamii868 <marcmotenii@gmail.com>
2025-03-27 17:34:10 -0500

    Add individual draft creation and error handling

--- server/draftTool/initializers/fearlessLobbyInitializer.ts
+++ server/draftTool/initializers/fearlessLobbyInitializer.ts
@@ -1,5 +1,8 @@
 import { randomUUID } from "crypto";
-import { insertInitialFearlessLobby } from "../../db/queries/insert";
+import {
+  insertDraft,
+  insertInitialFearlessLobby,
+} from "../../db/queries/insert";
 import {
   FearlessInitializerProps,
   FearlessStateServerProps,
@@ -18,60 +21,66 @@ export const fearlessLobbyInitializer = async ({
   redDisplayName,
   draftCount,
 }: FearlessInitializerProps) => {
-  const draftLobbies = [];
-
-  // Create draft lobbies
-  // Amout specified by draftCount variable from client
-  for (let i = 0; i < draftCount; i++) {
-    const lobbyCode = randomUUID();
-    const draftInfo = {
-      lobbyCode: lobbyCode,
-      blueUser: blueCode,
-      redUser: redCode,
-      blueDisplayName: blueDisplayName,
-      redDisplayName: redDisplayName,
-      tournamentID: null, //Can change later to track fearless for LBLCS
-      fearlessCode: fearlessCode,
-    };
-    const initializedDraft = fearlessDraftStateInitializer(draftInfo);
-    draftLobbies.push(initializedDraft);
+  try {
+    const draftLobbies = [];
+    // Create draft lobbies
+    // Amout specified by draftCount variable from client
+    for (let i = 0; i < draftCount; i++) {
+      const lobbyCode = randomUUID();
+      const draftInfo = {
+        lobbyCode: lobbyCode,
+        blueUser: blueCode,
+        redUser: redCode,
+        blueDisplayName: blueDisplayName,
+        redDisplayName: redDisplayName,
+        tournamentID: null, //Can change later to track fearless for LBLCS
+        fearlessCode: fearlessCode,
+      };
+      const initializedDraft = fearlessDraftStateInitializer(draftInfo);
+      await insertDraft(draftInfo);
+      draftLobbies.push(initializedDraft);
+    }
+  
+    // Verify draftLobbies array contents
+    if (!fearlessState[fearlessCode]) {
+      fearlessState[fearlessCode] = {
+        fearlessCode: fearlessCode,
+        fearlessComplete: false,
+        blueCode: blueCode,
+        redCode: redCode,
+        blueDisplayName: blueDisplayName,
+        redDisplayName: redDisplayName,
+        draftCount: draftCount,
+        completedDrafts: 0,
+        currentDraft: null,
+        allPicks: [],
+        allBans: [],
+        draftLobbyCodes: draftLobbies.map((lobby) => lobby.lobbyCode),
+      };
+  
+      // Save Fearless Lobby to Database
+      await insertInitialFearlessLobby({
+        fearlessCode,
+        blueCode,
+        redCode,
+        blueDisplayName,
+        redDisplayName,
+        draftCount,
+        draftLobbyCodes: draftLobbies.map((lobby) => lobby.lobbyCode),
+      });
+  
+      // Sets expiration in server record (Currently 24 hours)
+      setTimeout(() => {
+        if (fearlessState[fearlessCode]) {
+          delete fearlessState[fearlessCode];
+          console.log(`Fearless lobby ${fearlessCode} deleted`);
+        }
+      }, twentyFourHours);
+    }
+    return fearlessState[fearlessCode];
+  } catch (err) {
+    console.error("Error setting up Fearless Lobby: ", err)
+    throw new Error("Internal Server Error")
   }
 
-  // Verify draftLobbies array contents
-  if (!fearlessState[fearlessCode]) {
-    fearlessState[fearlessCode] = {
-      fearlessCode: fearlessCode,
-      fearlessComplete: false,
-      blueCode: blueCode,
-      redCode: redCode,
-      blueDisplayName: blueDisplayName,
-      redDisplayName: redDisplayName,
-      draftCount: draftCount,
-      completedDrafts: 0,
-      currentDraft: null,
-      allPicks: [],
-      allBans: [],
-      draftLobbyCodes: draftLobbies.map((lobby) => lobby.lobbyCode),
-    };
-
-    // Save Fearless Lobby to Database
-    await insertInitialFearlessLobby({
-      fearlessCode,
-      blueCode,
-      redCode,
-      blueDisplayName,
-      redDisplayName,
-      draftCount,
-      draftLobbyCodes: draftLobbies.map((lobby) => lobby.lobbyCode),
-    });
-
-    // Sets expiration in server record (Currently 24 hours)
-    setTimeout(() => {
-      if (fearlessState[fearlessCode]) {
-        delete fearlessState[fearlessCode];
-        console.log(`Fearless lobby ${fearlessCode} deleted`);
-      }
-    }, twentyFourHours);
-  }
-  return fearlessState[fearlessCode];
 };
