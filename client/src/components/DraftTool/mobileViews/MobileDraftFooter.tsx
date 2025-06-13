b8cddaa2a01ecd3c844248a8f21ad54464b561a0
Mamii868 <marcmotenii@gmail.com>
2025-06-13 14:39:40 -0500

    Create footer with champion pick tab

--- client/src/components/DraftTool/mobileViews/MobileDraftFooter.tsx
+++ client/src/components/DraftTool/mobileViews/MobileDraftFooter.tsx
@@ -1,7 +1,35 @@
-const MobileDraftFooter = () => {
+import DraftButton from "../draftViews/DraftButton";
+const MobileDraftFooter = ({
+  footerIsOpen,
+  setFooterIsOpen,
+}: {
+  footerIsOpen: boolean;
+  setFooterIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   return (
     <>
-      <div className="mobileFooter sticky w-full bottom-0 bg-orange h-12 z-10"></div>
+      <div className="mobileFooter sticky w-full bottom-0 bg-light-gray h-18 z-[52] flex flex-col items-center justify-center">
+        <div className="relative w-16">
+          <div
+            onClick={() => setFooterIsOpen(!footerIsOpen)}
+            className="champContainerPullup absolute -top-6 w-16 h-6 bg-black/80 flex items-center rounded-t-md"
+          >
+            <div
+              className={`line w-8 h-2 bg-white rounded translate-x-1 transition duration-300 ${
+                footerIsOpen ? "rotate-[20deg]" : "rotate-[-20deg]"
+              } `}
+            ></div>
+            <div
+              className={`line w-8 h-2 bg-white rounded -translate-x-1 transition duration-300 ${
+                footerIsOpen ? "rotate-[-20deg]" : "rotate-[20deg]"
+              }  `}
+            ></div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <DraftButton />
+        </div>
+      </div>
     </>
   );
 };
