724e6b475e6bfc4b9f22c03e93b715978c558e8f
Mamii868 <marcmotenii@gmail.com>
2025-06-28 15:50:30 -0500

    Move footer to fix placement

--- client/src/routes/DefaultRoutes.tsx
+++ client/src/routes/DefaultRoutes.tsx
@@ -11,26 +11,34 @@ import ASExecutive from "../components/AllStarsPage/ASExecutive";
 import StatsMain from "../components/StatsPage/StatsMain";
 import StatsSeason from "../components/StatsPage/StatsSeason";
 import ErrorPage from "../components/ErrorPage";
+import Footer from "../components/Footer";
 
 const DefaultRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="rosters" element={<Roster />} />
-      <Route path="rosters/:league" element={<LeaguePlayers />} />
-      <Route path="allstars" element={<ASMain />}>
-        <Route path="economy" element={<ASEconomy />} />
-        <Route path="commercial" element={<ASCommercial />} />
-        <Route path="financial" element={<ASFinancial />} />
-        <Route path="executive" element={<ASExecutive />} />
-      </Route>
-      <Route path="stats" element={<StatsMain />} />
-      {/* <Route path="stats/player/:player" element={<StatsPlayer/>}/> */}
-      <Route path="stats/team/" element={<StatsSeason />} />
-      {/* <Route path="stats/team/:team" element={<StatsTeamUI/>}/> */}
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <div className="grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="rosters" element={<Roster />} />
+          <Route path="rosters/:league" element={<LeaguePlayers />} />
+          <Route path="allstars" element={<ASMain />}>
+            <Route path="economy" element={<ASEconomy />} />
+            <Route path="commercial" element={<ASCommercial />} />
+            <Route path="financial" element={<ASFinancial />} />
+            <Route path="executive" element={<ASExecutive />} />
+          </Route>
+          <Route path="stats" element={<StatsMain />} />
+          {/* <Route path="stats/player/:player" element={<StatsPlayer/>}/> */}
+          <Route path="stats/team/" element={<StatsSeason />} />
+          {/* <Route path="stats/team/:team" element={<StatsTeamUI/>}/> */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+      <div className="flex-none">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
