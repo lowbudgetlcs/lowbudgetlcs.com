9d55ef2d2a73ab5797ec7357e9838c3a2eefe958
Mamii868 <marcmotenii@gmail.com>
2025-06-27 22:10:29 -0500

    Remove rogue console log

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -41,7 +41,6 @@ function App() {
       window.location.replace(newUrl);
     }
   }, [currentHost, pathname, subdomain]);
-  console.log(location.pathname)
   // If a redirect is happening, you might want to render null or a loading spinner
   // to prevent the rest of the app from rendering momentarily.
   if (!subdomain && pathname.startsWith("/draft")) {
