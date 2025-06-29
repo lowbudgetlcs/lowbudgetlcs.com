8b3f5b72f762174f4da03772fb3ba1302a2ddd02
Mamii868 <marcmotenii@gmail.com>
2025-06-29 09:14:13 -0500

    Create initial content component

---
+++ client/src/components/AllStarsPage/ASContent.tsx
@@ -0,0 +1,10 @@
+const ASContent = () => {
+  return (
+    <div className="ascontent pt-20">
+      <div className="navContainer"></div>
+      <div className="contentContainer"></div>
+    </div>
+  );
+};
+
+export default ASContent;
