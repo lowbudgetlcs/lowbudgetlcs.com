12092d5d642c74aa162afad7af5a4be48d9245f9
Mamii868 <marcmotenii@gmail.com>
2025-06-29 08:18:44 -0500

    Add buttons and animation

--- client/src/components/AllStarsPage/ASMain.tsx
+++ client/src/components/AllStarsPage/ASMain.tsx
@@ -1,79 +1,67 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
-import Button from "../Button";
+import ASTitlePopup from "./ASTitlePopup";
+import { useLocalStorageState } from "../../hooks/uselocalStorageState";
 
 function ASMain() {
-  const [activeLink, setActiveLink] = useState<string>();
-  const [popupOpen, setPopupOpen] = useState<boolean>(true);
-  const [isClosing, setIsClosing] = useState<boolean>(false);
-  const toggleActive = (navItem: string) => {
+  const [activeLink, setActiveLink] = useState<number>();
+  // const [sidebarShown, setSidebarShown] = useLocalStorageState<boolean>("ASsidebarShown", false);
+  // const [buttonsShown, setButtonsShown] = useLocalStorageState<boolean>("ASbuttonsShown", true);
+  const [sidebarShown, setSidebarShown] = useState<boolean>(false);
+  const [buttonsShown, setButtonsShown] = useState<boolean>(true);
+  const toggleActive = (navItem: number) => {
     setActiveLink(navItem);
   };
 
   useEffect(() => {
     let timer: number;
-    if (isClosing) {
+    if (activeLink) {
       timer = setTimeout(() => {
-        setPopupOpen(false);
-        setIsClosing(false);
+        setButtonsShown(false);
+        setSidebarShown(true);
       }, 500);
+      timer = setTimeout(() => {}, 1000);
     }
 
     return () => clearTimeout(timer);
-  }, [isClosing]);
+  }, [activeLink]);
 
   return (
-    <div className="allstars gap-2 bg-white text-black dark:bg-black dark:text-white font-serif flex-col items-center justify-center">
-      {/* Title Popup */}
+    <>
+      <ASTitlePopup />
       <div
-        className={`popup absolute w-screen h-screen bg-black/80 flex flex-col md:flex-row justify-around items-center p-2 sm:p-12 md:gap-12 transition duration-500 ${
-          popupOpen ? "" : "hidden"
-        } ${isClosing ? "opacity-0" : ""}`}
-      >
-        <h1 className="text-6xl lg:text-7xl text-nowrap text-center font-bold">
-          All Stars
-        </h1>
-        <div className="flex flex-col items-center md:w-1/2 xl:w-1/3">
-          <p className="summary text-lg md:text-xl">
-            After every LBLCS season, each team votes for players on who is the
-            best in their role. We then take those results and smash all these
-            stars into teams of their own and make them compete for charity!
-          </p>
-          <p className="text-lg md:text-xl pt-4 pb-4">
-            All proceeds gained from the stream go to the charity announced in
-            discord. Please donate to help the cause!
-          </p>
-          <p className="text-white/60 text-lg md:text-xl">
-            The 1st all-star teams play in the event. If people are not
-            available, 2nd and 3rd team members are asked to fill in.
-          </p>
-          <button
-            onClick={() => setIsClosing(true)}
-            className="py-4 px-8 m-4 bg-blue rounded-md hover:bg-orange transition duration-300 font-bold text-lg"
-          >
-            Take a look
-          </button>
-        </div>
-      </div>
-      {/* Navigation */}
-      <div className="ASNav flex flex-col items-center gap-4 p-4">
+        className={`allstars flex gap-2 bg-white text-black dark:bg-black dark:text-white flex-col items-center justify-center grow pt-20 transition duration-500 ${
+          buttonsShown ? "" : "hidden"
+        } ${activeLink ? "opacity-0" : ""}`}>
         <div className="title text-6xl">
-          <h2>Select a Season</h2>
+          <h2>All Stars: Select a Season</h2>
         </div>
-        <div className="seasonSelect flex-col md:grid grid-cols-2 gap-8">
-          <button
-            className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}
-          >
-            Season 14
-          </button>
-                    <button
-            className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}
-          >
-            Season 13
-          </button>
+        {/* Navigation */}
+        <div className={`ASNav flex flex-col items-center gap-4 px-4 justify-center grow `}>
+          <div className="seasonSelect flex-col md:grid grid-cols-2 gap-8">
+            <button
+              onClick={() => toggleActive(14)}
+              className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}>
+              Season 14
+            </button>
+            <button
+              onClick={() => toggleActive(13)}
+              className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}>
+              Season 13
+            </button>
+            <button
+              onClick={() => toggleActive(12)}
+              className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}>
+              Season 12
+            </button>
+            <button
+              onClick={() => toggleActive(11)}
+              className={`text-2xl font-bold bg-gray px-14 py-10 rounded-md hover:bg-orange transition duration-300`}>
+              Season 11
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
