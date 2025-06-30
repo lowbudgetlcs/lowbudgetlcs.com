c7116669b7a32a3f9377c3407db9127aaf728f3a
Mamii868 <marcmotenii@gmail.com>
2025-06-30 13:07:44 -0500

    Add database connection and post view
    Still bugged with the order

--- server/routes/allStarsRoutes.ts
+++ server/routes/allStarsRoutes.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 const allStarsRoutes = express.Router();
 import { getAllStarsPosts } from "../db/queries/select";
 
-allStarsRoutes.get("/api/posts", async (req: Request, res: Response) => {
+allStarsRoutes.get("/api/posts/:seasonId", async (req: Request, res: Response) => {
   try {
     const response = await getAllStarsPosts(Number(req.params.seasonId));
     res.json(response);
