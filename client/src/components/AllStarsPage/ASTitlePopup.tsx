10f6f14de38cdb5780fe1846f115a80d8b8b5fcd
Mamii868 <marcmotenii@gmail.com>
2025-07-01 11:18:05 -0500

    Add animation

--- client/src/components/AllStarsPage/ASTitlePopup.tsx
+++ client/src/components/AllStarsPage/ASTitlePopup.tsx
@@ -21,29 +21,27 @@ const ASTitlePopup = () => {
       <div
         className={`popup absolute w-screen h-screen bg-black/90 flex flex-col md:flex-row justify-around items-center p-2 sm:p-12 md:gap-12 transition duration-500 text-white ${
           popupOpen ? "" : "hidden"
-        } ${isClosing ? "opacity-0" : ""}`}
-      >
-        <h1 className="text-6xl lg:text-7xl text-nowrap text-center font-bold">
+        } ${isClosing ? "opacity-0" : ""}`}>
+        <h1 className="text-6xl lg:text-7xl text-nowrap text-center font-bold opacity-0 animate-slide-in-right">
           All Stars
         </h1>
-        <div className="flex flex-col items-center md:w-1/2 xl:w-1/3">
+        <div className="flex flex-col items-center md:w-1/2 xl:w-1/3 opacity-0 [animation-delay:500ms] animate-slide-in-left">
           <p className="summary text-lg md:text-xl">
-            After every LBLCS season, each team votes for players on who is the
-            best in their role. We then take those results and smash all these
-            stars into teams of their own and make them compete for charity!
+            After every LBLCS season, each team votes for players on who is the best in their role.
+            We then take those results and smash all these stars into teams of their own and make
+            them compete for charity!
           </p>
           <p className="text-orange text-lg md:text-xl pt-4 pb-4">
-            All proceeds gained from the stream go to the charity announced in
-            Discord. Please donate to help the cause!
+            All proceeds gained from the stream go to the charity announced in Discord. Please
+            donate to help the cause!
           </p>
           <p className="text-white/60 text-lg md:text-xl">
-            The 1st all-star teams play in the event. If people are not
-            available, 2nd and 3rd team members are asked to fill in.
+            The 1st all-star teams play in the event. If people are not available, 2nd and 3rd team
+            members are asked to fill in.
           </p>
           <button
             onClick={() => setIsClosing(true)}
-            className="py-4 px-8 m-4 bg-blue rounded-md hover:bg-orange transition duration-300 font-bold text-lg"
-          >
+            className="py-4 px-8 m-4 bg-blue rounded-md hover:bg-orange transition duration-300 font-bold text-lg">
             Take a look
           </button>
         </div>
