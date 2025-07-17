4385be33c5eed6ecdcb670192bf4d6bfd92f3968
Mamii868 <marcmotenii@gmail.com>
2025-07-17 18:34:54 -0500

    Add initial home page

--- client/src/components/MatchHistoryPage/MHHome.tsx
+++ client/src/components/MatchHistoryPage/MHHome.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import MHTitlePopup from "./MHTitlePopup";
+// import { useLocalStorageState } from "../../hooks/uselocalStorageState";
+
 const MHHome = () => {
+  const [popupShown, setPopupShown] = useState<boolean>(true);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className={`grow flex`}>
+      <div className={`${popupShown ? "" : "hidden"}`}>
+        <MHTitlePopup />
+      </div>
+      <div className="mhrecentsearches">
+        <h2 className="title">Match History: Recent Game Searches</h2>
+      </div>
+    </div>
   );
 };
 
---
+++ client/src/components/MatchHistoryPage/MHTitlePopup.tsx
@@ -0,0 +1,62 @@
+import { FormEvent, useEffect, useState } from "react";
+
+const MHTitlePopup = () => {
+  const [popupOpen, setPopupOpen] = useState<boolean>(true);
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+
+  useEffect(() => {
+    let timer: number;
+    if (isClosing) {
+      timer = setTimeout(() => {
+        setPopupOpen(false);
+        setIsClosing(false);
+      }, 500);
+    }
+
+    return () => clearTimeout(timer);
+  }, [isClosing]);
+
+  const handleFormSubmission = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("hello")
+  };
+
+  return (
+    <>
+      {/* Title Popup */}
+      <div
+        className={`mhpopup absolute w-screen h-screen bg-black bg-opacity-95 flex flex-col md:flex-row justify-around items-center p-2 sm:p-12 md:gap-12 transition duration-500 text-white ${
+          popupOpen ? "" : "hidden"
+        } ${isClosing ? "opacity-0" : ""}`}
+      >
+        <h1 className="text-6xl lg:text-7xl text-nowrap text-center font-bold opacity-0 [animation-delay:100ms] animate-slide-in-right">
+          Match History
+        </h1>
+        <div className="flex flex-col items-center md:w-1/2 xl:w-1/3 opacity-0 [animation-delay:500ms] animate-slide-in-left">
+          <p className="summary text-lg md:text-xl">
+            Search up your match history!
+          </p>
+          <p className="text-orange text-lg md:text-xl pt-4 pb-4">
+            Just enter in your Match ID below to search
+          </p>
+          <p className="text-white/60 text-lg md:text-xl">
+            You can find the match ID on the top right in the post-game screen
+            in the client.
+          </p>
+          <form onSubmit={handleFormSubmission}>
+            <input
+              type="text"
+              placeholder="Match ID"
+              className="px-2 py-2 text-2xl bg-gray text-white rounded-md mt-2"
+            ></input>
+            <button className="py-4 px-8 m-4 bg-blue rounded-md hover:bg-orange transition duration-300 font-bold text-lg">
+              Take a look
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MHTitlePopup;
