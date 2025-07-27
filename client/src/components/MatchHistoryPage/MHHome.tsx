cb759cb9be890feb0de640e2d8705250f45f6af7
Mamii868 <marcmotenii@gmail.com>
2025-07-27 13:07:00 -0500

    Add check for match ID in parameters

--- client/src/components/MatchHistoryPage/MHHome.tsx
+++ client/src/components/MatchHistoryPage/MHHome.tsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MHTitlePopup from "./MHTitlePopup";
+import { useParams } from "react-router-dom";
+import { useSessionStorageState } from "../../hooks/useSessionStorageState";
+import { MatchDto } from "./interfaces/MatchV5";
 // import { useLocalStorageState } from "../../hooks/uselocalStorageState";
 
 const MHHome = () => {
   const [popupShown, setPopupShown] = useState<boolean>(true);
-
+  const [matchData, setMatchData] = useSessionStorageState<
+    MatchDto | undefined
+  >("matchData", undefined);
+  const params = useParams();
+  useEffect(() => {
+    if (params.matchID && matchData) {
+      setPopupShown(false);
+    }
+  }, [params.matchID, matchData]);
   return (
     <div className={`grow flex`}>
       <div className={`${popupShown ? "" : "hidden"}`}>
         <MHTitlePopup />
       </div>
-      <div className="mhrecentsearches">
-        <h2 className="title">Match History: Recent Game Searches</h2>
-      </div>
+      {matchData && (
+        <div className={`matchContainer ${popupShown ? "hidden" : ""} text-white`}>
+          Here is match Data
+        </div>
+      )}
     </div>
   );
 };
