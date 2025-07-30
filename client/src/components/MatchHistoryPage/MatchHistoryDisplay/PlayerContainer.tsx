b7e87af86f23b92a46c4f859ce27bb94d55c2964
Mamii868 <marcmotenii@gmail.com>
2025-07-30 18:08:53 -0500

    Add item list

--- client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainer.tsx
+++ client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainer.tsx
@@ -1,16 +1,20 @@
 import { ParticipantDto } from "../interfaces/MatchV5";
-import PlayerInfo from "./RuneDisplay/PlayerInfo";
+import ItemDisplay from "./ItemDisplay";
+import PlayerInfo from "./PlayerInfo";
 import Runes from "./RuneDisplay/Runes";
 import SummonerSpellsDisplay from "./SummonerSpellsDisplay";
 
 const PlayerContainer = ({ playerData }: { playerData: ParticipantDto }) => {
   return (
-    <div className="playerContainer flex gap-2 items-center">
+    <div className="playerContainer flex gap-2 items-center bg-light-gray p-2 w-full rounded-md">
       <div className="champPlayerInfo flex gap-2 items-center justify-around">
         <PlayerInfo playerData={playerData} />
         <div className="playerBuild flex gap-2">
           <SummonerSpellsDisplay playerData={playerData} />
           <Runes playerData={playerData} />
+        </div>
+        <div className="itemDisplay">
+          <ItemDisplay playerData={playerData} />
         </div>
       </div>
     </div>
