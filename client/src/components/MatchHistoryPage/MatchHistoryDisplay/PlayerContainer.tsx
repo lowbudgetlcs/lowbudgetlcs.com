67b4c91af3ac93cc4a7510195d2adb92847d2d25
Mamii868 <marcmotenii@gmail.com>
2025-07-30 22:03:31 -0500

    Add labels to stats

--- client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainer.tsx
+++ client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainer.tsx
@@ -16,8 +16,8 @@ const PlayerContainer = ({
     ((playerData.kills + playerData.assists) / (totalKills / 100)).toFixed(0)
   );
   return (
-    <div className="playerContainer flex gap-2 items-center bg-light-gray p-2 w-full rounded-md">
-      <div className="champPlayerInfo flex gap-2 items-center grow">
+    <div className="playerContainer bg-light-gray p-2 w-full rounded-md">
+      <div className="champPlayerInfo grid grid-rows-1 grid-cols-6 gap-2 items-center">
         <PlayerInfo playerData={playerData} />
         <div className="playerBuild flex gap-2">
           <SummonerSpellsDisplay playerData={playerData} />
@@ -26,20 +26,34 @@ const PlayerContainer = ({
         <div className="itemDisplay">
           <ItemDisplay playerData={playerData} />
         </div>
-        <div className="playerStats flex text-2xl font-bold">
-          <p>{playerData.kills}</p>/<p className="text-red">{playerData.deaths}</p>/<p>{playerData.assists}</p>
+        <div className="kda flex flex-col items-center">
+          <div className="flex text-2xl font-bold justify-center">
+            <p>{playerData.kills}</p>&nbsp;/&nbsp;<p className="text-red">{playerData.deaths}</p>&nbsp;/&nbsp;
+            <p>{playerData.assists}</p>
+          </div>
+          <p className="text-white/60">K / D / A</p>
         </div>
-        <p className="cs">{playerData.totalMinionsKilled + playerData.neutralMinionsKilled}</p>
-        <p
-          className={`text-2xl font-bold ${
-            killParticipation > 50
-              ? "text-green"
-              : killParticipation < 50 && killParticipation > 30
-              ? "text-yellow"
-              : "text-red"
-          }`}>
-          {killParticipation}%
-        </p>
+
+        <div className="flex flex-col text-center">
+          <p className="cs text-2xl font-bold">
+            {playerData.totalMinionsKilled + playerData.neutralMinionsKilled}
+          </p>
+          <p className="text-white/60">CS</p>
+        </div>
+
+        <div className="flex flex-col text-center">
+          <p
+            className={`text-2xl font-bold ${
+              killParticipation > 50
+                ? "text-green"
+                : killParticipation < 50 && killParticipation > 30
+                ? "text-yellow"
+                : "text-red"
+            }`}>
+            {killParticipation}%
+          </p>
+          <p className="text-white/60">Kill Participation</p>
+        </div>
       </div>
     </div>
   );
