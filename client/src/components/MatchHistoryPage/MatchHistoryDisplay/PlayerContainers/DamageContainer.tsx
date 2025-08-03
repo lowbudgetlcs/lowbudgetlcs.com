9058b0ff3a8b245e9054ab5b72d9783ee3c74785
Mamii868 <marcmotenii@gmail.com>
2025-08-02 21:47:43 -0500

    add styling to graph

--- client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainers/DamageContainer.tsx
+++ client/src/components/MatchHistoryPage/MatchHistoryDisplay/PlayerContainers/DamageContainer.tsx
@@ -1,28 +1,70 @@
 import { Bar } from "react-chartjs-2";
 import { ParticipantDto } from "../../interfaces/MatchV5";
-import {Chart as ChartJS, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend} from "chart.js";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 const DamageContainer = ({ players }: { players: ParticipantDto[] }) => {
   const labels = players.map((player) => player.riotIdGameName);
-    const maxDamage = players.reduce((max, player) => Math.max(max, player.totalDamageDealtToChampions), 0);
+  const maxDamage = players.reduce(
+    (max, player) => Math.max(max, player.totalDamageDealtToChampions),
+    0
+  );
   ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-const damageList = players.map((player) => player.totalDamageDealtToChampions);
-console.log(damageList)
-const options = {
-  indexAxis: 'y' as const,
-  plugins: {
-    legend: {
-      position: 'right' as const,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const damageList = players.map(
+    (player) => player.totalDamageDealtToChampions
+  );
+  console.log(damageList);
+  const options = {
+    indexAxis: "y" as const,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+      tooltip: {
+        backgroundColor: "#252525",
+        bodyFont: {
+          size: 14,
+        } as const,
+        titleFont: {
+          weight: "bold",
+          size: 18,
+        } as const,
+      },
     },
-  },
-};
+    responsive: true,
+    scales: {
+      x: {
+        ticks: {
+          color: "gray",
+        },
+      },
+      y: {
+        ticks: {
+          color: "#F5F5F5",
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+  };
 
   const data = {
     labels: labels,
@@ -36,13 +78,8 @@ const options = {
     ],
   };
   return (
-    <div className="damageContainer flex">
-      <div className="playerName">
-        {players.map((player) => (
-          <p key={player.riotIdGameName}>{player.riotIdGameName}</p>
-        ))}
-      </div>
-      <Bar options={options} data={data} />
+    <div className="damageContainer flex max-w-[879.82px]">
+      <Bar options={options} data={data}/>
     </div>
   );
 };
