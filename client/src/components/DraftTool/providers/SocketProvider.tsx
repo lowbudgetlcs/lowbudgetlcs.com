f2a37124f6c1e78c01132751b6996827807a9c97
Mamii868 <marcmotenii@gmail.com>
2025-08-13 07:42:34 -0500

    Add state variables for socket connection
    This is to create popups when you connect to the socket

--- client/src/components/DraftTool/providers/SocketProvider.tsx
+++ client/src/components/DraftTool/providers/SocketProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 import { Socket, io } from "socket.io-client";
 import { getClientId } from "../../../utils/clientId";
 import { Outlet } from "react-router-dom";
@@ -7,37 +7,77 @@ interface SocketContextProps {
   createSocket: (namespace: string) => Socket;
   disconnectSocket: (socket: Socket) => void;
   clientId: string;
+  // Popups
+  showConnectedPopup: boolean;
+  setShowConnectedPopup: React.Dispatch<React.SetStateAction<boolean>>;
+  showReconnectPopup: boolean;
+  setShowReconnectPopup: React.Dispatch<React.SetStateAction<boolean>>;
+  showErrorPopup: boolean;
+  setShowErrorPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const SocketContext = createContext<SocketContextProps | undefined>(undefined);
 
 export const SocketProvider: React.FC = () => {
   const [activeSocketMap] = useState<Map<Socket, string>>(new Map());
+  const [showReconnectPopup, setShowReconnectPopup] = useState<boolean>(false);
+  const [showErrorPopup, setShowErrorPopup] = useState<boolean>(false);
+  const [showConnectedPopup, setShowConnectedPopup] = useState<boolean>(false);
+
   const clientId = getClientId();
-  
+
   // create a new socket with the client ID
-  const createSocket = (namespace: string): Socket => {
-    const newSocket = io(`${import.meta.env.VITE_BACKEND_URL}${namespace}`, {
-      query: { clientId },
-      reconnection: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 1000
-    });
-    
-    activeSocketMap.set(newSocket, namespace);
-    return newSocket;
-  };
-  
+  const createSocket = useCallback(
+    (namespace: string): Socket => {
+      const newSocket = io(`${import.meta.env.VITE_BACKEND_URL}${namespace}`, {
+        query: { clientId },
+        reconnection: true,
+        reconnectionAttempts: 5,
+        reconnectionDelay: 1000,
+      });
+
+      const connected = () => {
+        console.log(`Socket connected to ${namespace}`);
+        setShowConnectedPopup(true);
+      };
+      newSocket.on("connect", connected);
+      newSocket.on("disconnect", () => {
+        console.log(`Socket disconnected from ${namespace}`);
+        setShowConnectedPopup(false);
+        setShowReconnectPopup(true);
+      });
+      newSocket.on("connect_error", () => {
+        console.log(`Socket connection error to ${namespace}`);
+        setShowErrorPopup(true);
+      });
+
+      activeSocketMap.set(newSocket, namespace);
+      return newSocket;
+    },
+    [clientId]
+  );
+
   // Disconnects socket
-  const disconnectSocket = (socket: Socket) => {
+  const disconnectSocket = useCallback((socket: Socket) => {
     if (socket && activeSocketMap.has(socket)) {
       socket.disconnect();
       activeSocketMap.delete(socket);
     }
-  };
+  }, []);
   return (
-    <SocketContext.Provider value={{ createSocket, disconnectSocket, clientId }}>
-      <Outlet/>
+    <SocketContext.Provider
+      value={{
+        createSocket,
+        disconnectSocket,
+        clientId,
+        showConnectedPopup,
+        setShowConnectedPopup,
+        showReconnectPopup,
+        setShowReconnectPopup,
+        showErrorPopup,
+        setShowErrorPopup,
+      }}>
+      <Outlet />
     </SocketContext.Provider>
   );
 };
