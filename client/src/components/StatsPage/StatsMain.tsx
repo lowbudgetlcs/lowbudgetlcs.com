4170c6ae87e27f12742d4982bded4922b7f9090e
Mamii868 <marcmotenii@gmail.com>
2025-10-02 14:22:04 -0500

    Refactor initial main route

--- client/src/components/StatsPage/StatsMain.tsx
+++ client/src/components/StatsPage/StatsMain.tsx
@@ -1,68 +1,63 @@
-import { useState } from "react";
-import StatsSearchUI from "./StatsSearchUI";
-import StatsSeason from "./StatsSeason";
+import { NavLink } from "react-router-dom";
+import MiniGameCard from "./cards/MiniGameCard";
+import { useEffect, useState } from "react";
+import getRecentGames from "./dataHandlers/getRecentGames";
+import { RecentGame } from "../../types/StatTypes";
+const StatsMain = () => {
+  const [recentGames, setRecentGames] = useState<RecentGame[]>([]);
+  const [loading, setLoading] = useState(true);
 
-function StatsMain() {
-  const [isSearchActive, setIsSearchActive] = useState<Boolean>(false);
-  const [isTeamsActive, setIsTeamsActive] = useState<Boolean>(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getRecentGames(5);
+      if (response.length > 0) {
+        setRecentGames(response);
+        setLoading(false);
+      }
+    };
 
-  const displayStatOptions = () => {
-    if (isSearchActive) {
-      return <StatsSearchUI />;
-    } else if (isTeamsActive) {
-      return <StatsSeason />;
-    } else {
-      return (
-        <div className="statsMainContainer flex flex-col md:flex-row justify-center p-8 gap-8">
-          <div
-            onClick={() => setIsSearchActive(true)}
-            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
-          >
-            <h2 className="text-3xl text-white text-center font-semibold z-10">
-              Search Player
-            </h2>
-          </div>
-          <div
-            onClick={() => setIsTeamsActive(true)}
-            className={`relative card cursor-pointer bg-gradient-to-br from-orange to-black flex items-center justify-center w-full min-h-32 md:h-40 rounded-lg before:rounded-md before:bg-gray/80 before:dark:bg-light-gray before:z-0 before:absolute before:hover:opacity-0 before:w-full before:h-full before:transition-all before:duration-300`}
-          >
-            <h2 className="text-3xl text-white text-center font-semibold z-10">
-              Team Stats
-            </h2>
-          </div>
-        </div>
-      );
-    }
-  };
+    fetchData();
+  }, [])
   return (
-    <div className="aboutbg-white text-black dark:bg-black dark:text-white font-serif">
-      {isSearchActive || isTeamsActive ? (
-        <div
-          onClick={() => {
-            setIsSearchActive(false);
-            setIsTeamsActive(false);
-          }}
-          className="fixed flex z-50 my-2 px-2 rounded-lg top-1 left-16 text-2xl font-semibold cursor-pointer w-fit h-fit justify-center items-center  group"
-        >
-          <div className="burger cursor-pointer relative h-12 w-6 gap-1 hover:cursor-pointer self-baseline">
-            <div
-              className={`absolute -rotate-45 top-5 left-0 transition-all duration-300 px-2 py-0.5 rounded-xl bg-white group-hover:bg-orange`}
-            ></div>
-            <div
-              className={`absolute rotate-45 top-7 left-0 transition-all duration-300 px-2 py-0.5 rounded-xl bg-white group-hover:bg-orange`}
-            ></div>
-          </div>
-          <p className="group-hover:text-orange underline transition duration-300 ">
-            Back
-          </p>
-        </div>
-      ) : (
-        ""
-      )}
-      <div className="title h-64 w-full flex items-center justify-center">
-        <h1 className="text-6xl text-center">LBLCS Stats</h1>
+    <div className="flex gap-2 bg-white text-black dark:bg-black dark:text-white grow pt-20 transition duration-500 items-center justify-around">
+      <div
+        className={`bg-light-gray rounded-md border-2 p-4 md:p-12 border-gray bg-opacity-95 flex flex-col items-center transition duration-500 text-white`}>
+        <h1 className="text-4xl text-center font-bold opacity-0 p-4 [animation-delay:100ms] animate-slide-in-right">
+          Stats
+        </h1>
+        <ul className="flex flex-col items-center opacity-0 gap-4 [animation-delay:300ms] animate-slide-in-right">
+          <NavLink
+            to={"/#"}
+            className={`text-xl font-bold w-52 text-center bg-gray px-8 py-8 rounded-md hover:bg-orange transition duration-300`}>
+            <li>Divisions</li>
+          </NavLink>
+          <NavLink
+            to={"/#"}
+            className={`text-xl font-bold w-52 text-center bg-gray px-8 py-8 rounded-md hover:bg-orange transition duration-300`}>
+            <li>Teams</li>
+          </NavLink>
+          <NavLink
+            to={"/#"}
+            className={`text-xl font-bold w-52 text-center bg-gray px-8 py-8 rounded-md hover:bg-orange transition duration-300`}>
+            <li>Player Search</li>
+          </NavLink>
+        </ul>
       </div>
-      { displayStatOptions()}
+      <div
+        className={`bg-light-gray rounded-md border-2 p-4 md:p-12 border-gray flex flex-col items-center transition text-white`}>
+        <h2 className="text-3xl text-center font-bold opacity-0 p-4 [animation-delay:100ms] animate-slide-in-right">
+          Recent Games
+        </h2>
+        <div className="flex flex-col items-center">
+          {loading ? (
+            <div className="animate-spin border-b-2 border-r-2 border-t-2 border-orange rounded-full p-4 w-24 h-24"></div>
+          ) : (
+            recentGames.map((game, index) => (
+              <MiniGameCard key={index} game={game} />
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/routes/StatRoutes.tsx
+++ client/src/routes/StatRoutes.tsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import StatsMain from "../components/StatsPage/StatsMain";
 import StatsSeason from "../components/StatsPage/StatsSeason";
+import Footer from "../components/Footer";
 
 const StatRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<StatsMain />} />
-      <Route path="stats/team/" element={<StatsSeason />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Routes>
+        <Route path="/" element={<StatsMain />} />
+        <Route path="stats/team/" element={<StatsSeason />} />
+      </Routes>
+      <div className="flex-none">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
