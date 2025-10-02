7bb3425a6b7313a26c665b29076f7d01d770fa9c
Mamii868 <marcmotenii@gmail.com>
2025-10-02 12:01:54 -0500

    Remove unused routes

--- client/src/routes/DefaultRoutes.tsx
+++ client/src/routes/DefaultRoutes.tsx
@@ -4,8 +4,6 @@ import About from "../components/AboutPage/About";
 import Roster from "../components/RosterPage/Roster";
 import LeaguePlayers from "../components/RosterPage/LeaguePlayers";
 import ASMain from "../components/AllStarsPage/ASMain";
-import StatsMain from "../components/StatsPage/StatsMain";
-import StatsSeason from "../components/StatsPage/StatsSeason";
 import ErrorPage from "../components/ErrorPage";
 import Footer from "../components/Footer";
 import MHHome from "../components/MatchHistoryPage/MHHome";
@@ -22,10 +20,6 @@ const DefaultRoutes = () => {
           <Route path="allstars" element={<ASMain />} />
           <Route path="mh" element={<MHHome />} />
           <Route path="mh/:matchID" element={<MHHome />} />
-          <Route path="stats" element={<StatsMain />} />
-          {/* <Route path="stats/player/:player" element={<StatsPlayer/>}/> */}
-          <Route path="stats/team/" element={<StatsSeason />} />
-          {/* <Route path="stats/team/:team" element={<StatsTeamUI/>}/> */}
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </div>
