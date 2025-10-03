309a7babc192f2809c9e869d3fff074c3ef85506
Mamii868 <marcmotenii@gmail.com>
2025-10-02 21:48:39 -0500

    Add route

--- client/src/routes/StatRoutes.tsx
+++ client/src/routes/StatRoutes.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import StatsMain from "../components/StatsPage/StatsMain";
 import StatsSeason from "../components/StatsPage/StatsSeason";
 import Footer from "../components/Footer";
+import StatsPlayer from "../components/StatsPage/PlayerStatDisplay/StatsPlayer";
 
 const StatRoutes = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Routes>
         <Route path="/" element={<StatsMain />} />
-        <Route path="stats/team/" element={<StatsSeason />} />
+        <Route path="team/" element={<StatsSeason />} />
+        <Route path="player/:summonerName" element={<StatsPlayer />} />
       </Routes>
       <div className="flex-none">
         <Footer />
