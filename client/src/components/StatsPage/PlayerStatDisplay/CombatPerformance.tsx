83a621efd8bf788e34ac92f82f3748c4dcc5fd00
Mamii868 <marcmotenii@gmail.com>
2025-10-06 06:55:34 -0500

    Add damage share

--- client/src/components/StatsPage/PlayerStatDisplay/CombatPerformance.tsx
+++ client/src/components/StatsPage/PlayerStatDisplay/CombatPerformance.tsx
@@ -1,8 +1,8 @@
 import { FaHandshake, FaSkull } from "react-icons/fa";
 import IndividualStatCard from "../cards/IndividualStatCard";
 import { PlayerOverallStats } from "../../../types/StatTypes";
-import { LuSwords } from "react-icons/lu";
 import { GiMineExplosion } from "react-icons/gi";
+import { IoPieChart } from "react-icons/io5";
 
 const CombatPerformance = ({ playerData }: { playerData: PlayerOverallStats }) => {
   return (
@@ -10,15 +10,13 @@ const CombatPerformance = ({ playerData }: { playerData: PlayerOverallStats }) =
       <h2 className="text-2xl font-bold border-b-2 border-white/60 mb-4">Combat</h2>
       {/* Stat Boxes */}
       <div className="smallStatBoxes flex flex-col gap-4">
-        {/* KDA */}
+        {/* KP */}
         <IndividualStatCard
-          icon={<LuSwords className="text-white w-[25px] h-[25px]" />}
-          iconBgColor="bg-red bg-opacity-50"
-          title="KDA Ratio"
-          value={playerData.kda.toFixed(2)}
-          valueColor={
-            playerData.kda >= 2 ? "text-blue" : playerData.kda <= 1 ? "text-red" : "text-white"
-          }
+          icon={<FaHandshake className="text-white w-[25px] h-[25px]" />}
+          iconBgColor="bg-pink-500 bg-opacity-50"
+          title="Kill Participation"
+          value={playerData.avgKillParticipation.toFixed(0) + "%"}
+          valueColor={playerData.avgKillParticipation >= 49.5 ? "text-blue" : "text-red"}
         />
         {/* Damage/Min */}
         <IndividualStatCard
@@ -27,13 +25,12 @@ const CombatPerformance = ({ playerData }: { playerData: PlayerOverallStats }) =
           title="DMG/Min"
           value={playerData.avgDamagePerMin.toFixed(0)}
         />
-        {/* KP */}
+        {/* Damage Share */}
         <IndividualStatCard
-          icon={<FaHandshake className="text-white w-[25px] h-[25px]" />}
-          iconBgColor="bg-pink-500 bg-opacity-50"
-          title="Kill Participation"
-          value={playerData.avgKillParticipation.toFixed(0) + "%"}
-          valueColor={playerData.avgKillParticipation >= 49.5 ? "text-blue" : "text-red"}
+          icon={<IoPieChart className="text-white w-[25px] h-[25px]" />}
+          iconBgColor="bg-red bg-opacity-50"
+          title="Damage Share"
+          value={playerData.avgDamageShare.toFixed(0)+"%"}
         />
         {/* Deaths/Game */}
         <IndividualStatCard
