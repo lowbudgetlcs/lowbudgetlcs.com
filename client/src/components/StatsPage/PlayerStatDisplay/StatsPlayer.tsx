2636c27e717344fec44a6f584f85b9d1c6858826
Mamii868 <marcmotenii@gmail.com>
2025-10-07 17:18:46 -0500

    Add loading view

--- client/src/components/StatsPage/PlayerStatDisplay/StatsPlayer.tsx
+++ client/src/components/StatsPage/PlayerStatDisplay/StatsPlayer.tsx
@@ -11,6 +11,7 @@ import EconPerformance from "./EconPerformance";
 import NavList from "../../NavList";
 import getPlayerGames from "../dataHandlers/getPlayerGames";
 import PlayerGameCard from "../cards/PlayerGameCard";
+import LoadingIcon from "../../LoadingIcon";
 
 function StatsPlayer() {
   const params = useParams();
@@ -54,7 +55,9 @@ function StatsPlayer() {
         setError(err.message);
         console.error("Error fetching player data:", err);
       } finally {
-        setLoading(false);
+        if (playerGames.length > 0) {
+          setLoading(false);
+        }
       }
     };
 
@@ -81,15 +84,8 @@ function StatsPlayer() {
     };
     fetchPlayerMatches();
   }, [playerData, summonerName, tagLine]);
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen bg-black text-white text-xl">
-        Loading player stats...
-      </div>
-    );
-  }
 
-  if (error || !playerData) {
+  if (error) {
     navigate("/");
     return null;
   }
@@ -110,47 +106,55 @@ function StatsPlayer() {
       <div className="flex flex-col md:flex-row justify-stretch md:p-4 gap-8">
         {/* Stat Sidebar */}
         <PlayerStatSidebar summonerName={summonerName} tagLine={tagLine} playerData={playerData} />
-        <div className="extendedStatsContainer flex flex-col gap-4 flex-grow p-4 border-2 border-gray rounded-md">
-          {/* Achievements */}
-          <AchievementsDisplay />
-          {/* Performance Overview */}
-          <PerformanceOverview playerData={playerData} />
-          <NavList activeLink={activeLink} toggleActive={toggleActive} navItems={navItems} />
-          {activeLink === "Overview" ? (
-            <>
-              <div className="performanceOverview flex flex-col sm:flex-row gap-2">
-                <CombatPerformance playerData={playerData} />
-                <EconPerformance playerData={playerData} />
-              </div>
-              {/* Champion Stats */}
-              <div className="specificChampStats">
-                <h2 className="text-2xl font-bold border-b-2 border-white/60 mb-4">
-                  Champion Stats
-                </h2>
-                <div className="champStats flex flex-col gap-4 max-h-72 overflow-y-auto">
-                  {/* Champion Box */}
-                  {/* Make it autofill with information from data */}
-                  {playerData.championPool
-                    .slice(0, 6)
-                    .sort((a, b) => b.games - a.games)
-                    .map((champ) => (
-                      <ChampionStatCard champ={champ} />
-                    ))}
-                </div>
-              </div>
-            </>
-          ) : activeLink === "Recent Games" ? (
-            <div className="recentGames">
-              <h2 className="text-2xl font-bold border-b-2 border-white/60 mb-4">Games</h2>
-              <div className="flex flex-col gap-2 items-center min-h-64">
-                {playerGames.length > 0 ? (
-                  playerGames.map((game, index) => <PlayerGameCard key={index} game={game} />)
-                ) : (
-                  <p className="text-xl text-white">No recent games found.</p>
-                )}
-              </div>
+        <div className="extendedStatsContainer flex flex-col gap-4 flex-grow p-4 border-2 border-gray rounded-md min-h-64">
+          {!playerData || loading ? (
+            <div className="loading min-w-64 flex items-center justify-center">
+              <LoadingIcon />
             </div>
-          ) : null}
+          ) : (
+            <>
+              {/* Achievements */}
+              <AchievementsDisplay />
+              {/* Performance Overview */}
+              <PerformanceOverview playerData={playerData} />
+              <NavList activeLink={activeLink} toggleActive={toggleActive} navItems={navItems} />
+              {activeLink === "Overview" ? (
+                <>
+                  <div className="performanceOverview flex flex-col sm:flex-row gap-2">
+                    <CombatPerformance playerData={playerData} />
+                    <EconPerformance playerData={playerData} />
+                  </div>
+                  {/* Champion Stats */}
+                  <div className="specificChampStats">
+                    <h2 className="text-2xl font-bold border-b-2 border-white/60 mb-4">
+                      Champion Stats
+                    </h2>
+                    <div className="champStats flex flex-col gap-4 max-h-72 overflow-y-auto">
+                      {/* Champion Box */}
+                      {/* Make it autofill with information from data */}
+                      {playerData.championPool
+                        .slice(0, 6)
+                        .sort((a, b) => b.games - a.games)
+                        .map((champ) => (
+                          <ChampionStatCard champ={champ} />
+                        ))}
+                    </div>
+                  </div>
+                </>
+              ) : activeLink === "Recent Games" ? (
+                <div className="recentGames">
+                  <h2 className="text-2xl font-bold border-b-2 border-white/60 mb-4">Games</h2>
+                  <div className="flex flex-col gap-2 items-center min-h-64">
+                    {playerGames.length > 0 ? (
+                      playerGames.map((game, index) => <PlayerGameCard key={index} game={game} />)
+                    ) : (
+                      <p className="text-xl text-white">No recent games found.</p>
+                    )}
+                  </div>
+                </div>
+              ) : null}
+            </>
+          )}
         </div>
       </div>
     </div>
