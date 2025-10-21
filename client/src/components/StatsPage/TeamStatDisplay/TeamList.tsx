29abd9b20b661bddb597ac07c54ed98bc94f4f0c
Mamii868 <marcmotenii@gmail.com>
2025-10-21 05:47:43 -0500

    Add team links and list sorting

--- client/src/components/StatsPage/TeamStatDisplay/TeamList.tsx
+++ client/src/components/StatsPage/TeamStatDisplay/TeamList.tsx
@@ -3,7 +3,7 @@ import getTeamsBySeason from "../dataHandlers/getTeamsBySeason";
 import NavList from "../../NavList";
 import { useQuery } from "@tanstack/react-query";
 import LoadingIcon from "../../LoadingIcon";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const TeamList = ({ activeSeason }: { activeSeason: number }) => {
   const navigate = useNavigate();
@@ -35,39 +35,43 @@ const TeamList = ({ activeSeason }: { activeSeason: number }) => {
   }
 
   if (isSuccess && navItems.length === 0) {
-    setNavItems(data.divisions.map((division) => division.divisionName));
+    const sortedDivisions = data.divisions.sort((a, b) => b.eventId - a.eventId)
+    setNavItems(sortedDivisions.map((division) => division.divisionName));
   }
   return (
-    <div className="flex flex-col teamList items-center justify-center text-white mt-20">
+    <div className="flex flex-col teamList items-center text-white mt-20 grow min-h-[80vh]">
+      <div className="w-full">
         <NavList activeLink={activeLink} toggleActive={toggleActive} navItems={navItems} />
-      <div className="flex flex-col md:grid grid-cols-2 gap-4 p-4">
+      </div>
+      <div className="flex flex-col md:grid grid-cols-2 xl:grid-cols-3 gap-4 p-4">
         {data.teams.map((team) => {
           if (team.divisions.divisionName === activeLink)
             return (
-              <div
+              <Link
+                to={`${team.teams.id}`}
                 key={team.teams.id}
-                className={`teamCard relative transition duration-300 rounded-md bg-gray/80 dark:bg-gray/40`}>
-                <div className="flex flex-col md:flex-row py-4 md:py-0 md:pl-4 gap-4 items-center max-w-md md:max-w-full md:w-full h-32 overflow-hidden">
+                className={`teamCard relative transition duration-300 rounded-md bg-gray/80 dark:bg-gray/40 w-64 lg:w-[24rem]`}>
+                <div className="flex flex-col md:flex-row py-2 md:py-0 md:pl-4 gap-2 items-center justify-center max-w-md md:max-w-full md:w-full h-40 md:h-32 overflow-hidden">
                   <div className="logoContainer flex flex-col md:flex-row gap-4 w-full md:w-auto md:h-full items-center">
                     {team.teams.logo ? (
                       <img
                         src={team.teams.logo}
-                        className="logo flex-shrink-0 w-[100px] text-center text-xl h-[100px]"
+                        className="logo flex-shrink-0 w-[80px] text-center text-xl h-[80px]"
                       />
                     ) : (
-                      <div className="logo flex-shrink-0 w-[100px] text-center text-xl h-[100px] bg-gray">
+                      <div className="logo flex-shrink-0 w-[80px] text-center text-xl h-[80px] bg-gray">
                         No logo 😢
                       </div>
                     )}
-                    <div className={`w-full h-3 md:w-3 md:h-full bg-orange`}></div>
+                    <div className={`w-full h-1 md:w-1 md:h-full bg-orange`}></div>
                   </div>
                   <div className="flex flex-col md:flex-row flex-1 flex-shrink items-center">
-                    <h3 className="teamName text-xl text-center md:text-left font-semibold px-4 md:px-4">
+                    <h3 className="teamName text-lg text-center md:text-left font-semibold">
                       {team.teams.teamName}
                     </h3>
                   </div>
                 </div>
-              </div>
+              </Link>
             );
         })}
       </div>
