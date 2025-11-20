c37bbf40c4370b55b9688e9da795b899366d0f0f
Mamii868 <marcmotenii@gmail.com>
2025-11-20 16:32:17 -0600

    Add cache to team list

--- server/routes/statsRoutes.ts
+++ server/routes/statsRoutes.ts
@@ -12,10 +12,7 @@ import { getTeamIdByName } from "../db/queries/select";
 import playerStatsAggregation from "../stats/playerStatsAggregation";
 import teamStatsAggregation from "../stats/teamStatsAggregation";
 import { EventWithTeamsDto } from "./rosterRoutes";
-import {
-  getDivisionsForSeason,
-  getDivisionsForSelectedSeason,
-} from "../db/queries/select";
+import { getDivisionsForSeason, getDivisionsForSelectedSeason } from "../db/queries/select";
 
 const statRoutes = express.Router();
 
@@ -39,135 +36,112 @@ interface TeamResponse {
   } | null;
 }
 // Get recent game stats from db
-statRoutes.get(
-  "/api/games/recent/:amount",
-  async (req: Request, res: Response) => {
-    try {
-      const amount: number = Number(req.params.amount);
-      if (isNaN(amount) || amount <= 0) {
-        return res.status(400).json({ error: "Invalid amount parameter" });
-      }
-      const response = await getRecentGames(amount);
-      if (response.length <= 0) {
-        return res.status(404).json({ error: "Matches Not Found" });
-      }
-      return res.json(response);
-    } catch (err: any) {
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/games/recent/:amount", async (req: Request, res: Response) => {
+  try {
+    const amount: number = Number(req.params.amount);
+    if (isNaN(amount) || amount <= 0) {
+      return res.status(400).json({ error: "Invalid amount parameter" });
     }
+    const response = await getRecentGames(amount);
+    if (response.length <= 0) {
+      return res.status(404).json({ error: "Matches Not Found" });
+    }
+    return res.json(response);
+  } catch (err: any) {
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 // Get recent games for a division
-statRoutes.get(
-  "/api/games/division/:divisionId/:amount",
-  async (req: Request, res: Response) => {
-    try {
-      const divisionId: number = Number(req.params.divisionId);
-      const amount: number = Number(req.params.amount);
-      if (
-        isNaN(divisionId) ||
-        isNaN(amount) ||
-        divisionId <= 0 ||
-        amount <= 0
-      ) {
-        return res.status(400).json({ error: "Invalid parameters" });
-      }
-      const response = await getRecentGamesByDivision(amount, divisionId);
-      if (response.length <= 0) {
-        return res.status(404).json({ error: "Matches Not Found" });
-      }
-      return res.json(response);
-    } catch (err: any) {
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/games/division/:divisionId/:amount", async (req: Request, res: Response) => {
+  try {
+    const divisionId: number = Number(req.params.divisionId);
+    const amount: number = Number(req.params.amount);
+    if (isNaN(divisionId) || isNaN(amount) || divisionId <= 0 || amount <= 0) {
+      return res.status(400).json({ error: "Invalid parameters" });
     }
+    const response = await getRecentGamesByDivision(amount, divisionId);
+    if (response.length <= 0) {
+      return res.status(404).json({ error: "Matches Not Found" });
+    }
+    return res.json(response);
+  } catch (err: any) {
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 // Get all games for a team
-statRoutes.get(
-  "/api/games/team/:teamId",
-  async (req: Request, res: Response) => {
-    try {
-      const teamId: number = Number(req.params.teamId);
-      if (isNaN(teamId) || teamId <= 0) {
-        return res.status(400).json({ error: "Invalid team ID" });
-      }
-      const response = await getGamesForTeam(teamId);
-      if (response.length <= 0) {
-        return res.status(404).json({ error: "Matches Not Found" });
-      }
-      return res.json(response);
-    } catch (err: any) {
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/games/team/:teamId", async (req: Request, res: Response) => {
+  try {
+    const teamId: number = Number(req.params.teamId);
+    if (isNaN(teamId) || teamId <= 0) {
+      return res.status(400).json({ error: "Invalid team ID" });
     }
+    const response = await getGamesForTeam(teamId);
+    if (response.length <= 0) {
+      return res.status(404).json({ error: "Matches Not Found" });
+    }
+    return res.json(response);
+  } catch (err: any) {
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 // Get all games for a player
-statRoutes.get(
-  "/api/games/player/:summonerName/:tagline",
-  async (req: Request, res: Response) => {
-    try {
-      const summonerName: string = req.params.summonerName;
-      const tagline: string = req.params.tagline;
-      const puuidResponse = await getPlayer(summonerName, tagline);
-      if (!puuidResponse || !puuidResponse.puuid) {
-        return res.status(404).json({ error: "Player Not Found" });
-      }
-
-      const response = await getGamesForPlayer(puuidResponse.puuid);
-      if (response.length <= 0) {
-        return res.status(404).json({ error: "Matches Not Found" });
-      }
-
-      return res.json(response);
-    } catch (err: any) {
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/games/player/:summonerName/:tagline", async (req: Request, res: Response) => {
+  try {
+    const summonerName: string = req.params.summonerName;
+    const tagline: string = req.params.tagline;
+    const puuidResponse = await getPlayer(summonerName, tagline);
+    if (!puuidResponse || !puuidResponse.puuid) {
+      return res.status(404).json({ error: "Player Not Found" });
     }
+
+    const response = await getGamesForPlayer(puuidResponse.puuid);
+    if (response.length <= 0) {
+      return res.status(404).json({ error: "Matches Not Found" });
+    }
+
+    return res.json(response);
+  } catch (err: any) {
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 // Get player overall stats by summonerID
-statRoutes.get(
-  "/api/player/summoner/:summonerName/:tagline",
-  async (req: Request, res: Response) => {
-    try {
-      const summonerName: string = req.params.summonerName;
-      const tagline: string = req.params.tagline;
-      const playerResponse = await getPlayer(summonerName, tagline);
-      if (!playerResponse) {
-        return res.status(404).json({ error: "Player Not Found" });
-      }
-      const puuid = playerResponse.puuid;
-      const overallStats = await playerStatsAggregation(puuid);
-      if (!overallStats) {
-        return res.status(404).json({ error: "Player Stats Not Found" });
-      }
-      return res.json(overallStats);
-    } catch (err: any) {
-      console.error("Error fetching player stats by name:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/player/summoner/:summonerName/:tagline", async (req: Request, res: Response) => {
+  try {
+    const summonerName: string = req.params.summonerName;
+    const tagline: string = req.params.tagline;
+    const playerResponse = await getPlayer(summonerName, tagline);
+    if (!playerResponse) {
+      return res.status(404).json({ error: "Player Not Found" });
     }
+    const puuid = playerResponse.puuid;
+    const overallStats = await playerStatsAggregation(puuid);
+    if (!overallStats) {
+      return res.status(404).json({ error: "Player Stats Not Found" });
+    }
+    return res.json(overallStats);
+  } catch (err: any) {
+    console.error("Error fetching player stats by name:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 // Get player stats by puuid
-statRoutes.get(
-  "/api/player/puuid/:puuid",
-  async (req: Request, res: Response) => {
-    try {
-      const puuid: string = req.params.puuid;
-      const overallStats = await playerStatsAggregation(puuid);
-      if (!overallStats) {
-        return res.status(404).json({ error: "Player Stats Not Found" });
-      }
-      return res.json(overallStats);
-    } catch (err: any) {
-      console.error("Error fetching player stats puuid:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/player/puuid/:puuid", async (req: Request, res: Response) => {
+  try {
+    const puuid: string = req.params.puuid;
+    const overallStats = await playerStatsAggregation(puuid);
+    if (!overallStats) {
+      return res.status(404).json({ error: "Player Stats Not Found" });
     }
+    return res.json(overallStats);
+  } catch (err: any) {
+    console.error("Error fetching player stats puuid:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 // Get team overall stats
 statRoutes.get("/api/team/:teamId", async (req: Request, res: Response) => {
@@ -213,7 +187,9 @@ statRoutes.get("/api/team/name/:teamName", async (req: Request, res: Response) =
           );
           if (!dennysApiResponse.ok) continue;
           const dennysApiEventData: EventWithTeamsDto = await dennysApiResponse.json();
-          const matched = dennysApiEventData.teams.find((t) => t.name.toLowerCase() === teamName.toLowerCase());
+          const matched = dennysApiEventData.teams.find(
+            (t) => t.name.toLowerCase() === teamName.toLowerCase()
+          );
           if (matched?.logoName) {
             teamLogo = matched.logoName;
             break;
@@ -233,23 +209,20 @@ statRoutes.get("/api/team/name/:teamName", async (req: Request, res: Response) =
 });
 
 // Check if a player is in the database
-statRoutes.get(
-  "/api/player/check/:summonerName/:tagline",
-  async (req: Request, res: Response) => {
-    try {
-      const summonerName: string = req.params.summonerName;
-      const tagline: string = req.params.tagline;
-      const playerResponse = await getPlayer(summonerName, tagline);
-      if (!playerResponse) {
-        return res.status(404).json({ found: false });
-      }
-      return res.status(200).json({ found: true, puuid: playerResponse.puuid });
-    } catch (err: any) {
-      console.error("Error checking player existence:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
+statRoutes.get("/api/player/check/:summonerName/:tagline", async (req: Request, res: Response) => {
+  try {
+    const summonerName: string = req.params.summonerName;
+    const tagline: string = req.params.tagline;
+    const playerResponse = await getPlayer(summonerName, tagline);
+    if (!playerResponse) {
+      return res.status(404).json({ found: false });
     }
+    return res.status(200).json({ found: true, puuid: playerResponse.puuid });
+  } catch (err: any) {
+    console.error("Error checking player existence:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-);
+});
 
 statRoutes.get("/api/seasons", async (req: Request, res: Response) => {
   try {
@@ -264,10 +237,16 @@ statRoutes.get("/api/seasons", async (req: Request, res: Response) => {
   }
 });
 
-statRoutes.get(
-  "/api/seasons/:seasonId",
-  async (req: Request, res: Response) => {
+// Cache map for teams and divisions for a season
+const seasonCache = new Map<number, any>();
+statRoutes.get("/api/seasons/:seasonId", async (req: Request, res: Response) => {
+  // 10 hours
+  const seasonCacheTTL = 10 * 60 * 60 * 1000;
+  if (seasonCache.has(Number(req.params.seasonId))) {
+    return res.status(200).json(seasonCache.get(Number(req.params.seasonId)));
+  } else {
     try {
+      console.log(`Fetching season data for season ID ${req.params.seasonId} from DB.`);
       const seasonId: number = Number(req.params.seasonId);
       if (isNaN(seasonId) || seasonId <= 0) {
         return res.status(400).json({ error: "Invalid season ID" });
@@ -284,8 +263,7 @@ statRoutes.get(
             `https://dennys.lowbudgetlcs.com/api/v1/event/${team.divisions.eventId}/teams`
           );
           if (dennysApiResponse) {
-            const dennysApiEventData: EventWithTeamsDto =
-              await dennysApiResponse.json();
+            const dennysApiEventData: EventWithTeamsDto = await dennysApiResponse.json();
             const matchedTeam = dennysApiEventData.teams.find(
               (t) => t.name.toLowerCase() === team.teams?.teamName.toLowerCase()
             );
@@ -301,15 +279,18 @@ statRoutes.get(
       if (divisionsResponse.length <= 0) {
         return res.status(404).json({ error: "Season Not Found" });
       }
-      return res.json({
-        divisions: divisionsResponse,
-        teams: teamsResponse,
-      });
+      // Set cache to avoid repeated calls within TTL
+      seasonCache.set(seasonId, { teams: teamsResponse, divisions: divisionsResponse });
+      setTimeout(() => {
+        seasonCache.delete(seasonId);
+      }, seasonCacheTTL);
+      console.log(`Cached season data for season ID ${seasonId} for 10 hours.`);
+      return res.json({ teams: teamsResponse, divisions: divisionsResponse });
     } catch (err: any) {
       console.error("Error getting season by ID:", err);
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
-);
+});
 
 export default statRoutes;
