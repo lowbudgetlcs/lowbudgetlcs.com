22476d25000f8ec5c02c4a20b0905dda03c1efd2
Mamii868 <marcmotenii@gmail.com>
2025-11-21 18:04:17 -0600

    Add better icons

--- client/src/components/StatsPage/TeamStatDisplay/ObjectiveControl.tsx
+++ client/src/components/StatsPage/TeamStatDisplay/ObjectiveControl.tsx
@@ -1,8 +1,12 @@
 import { TeamOverallStats } from "../../../types/StatTypes";
-import { GiMineExplosion } from "react-icons/gi";
-import { FaCoins } from "react-icons/fa";
-import { IoPieChart } from "react-icons/io5";
-import { IoLogoGameControllerA } from "react-icons/io";
+import {
+  GiChessRook,
+  GiCurledTentacle,
+  GiLeechingWorm,
+  GiRose,
+  GiSwirledShell,
+} from "react-icons/gi";
+import { FaDragon } from "react-icons/fa6";
 
 const ObjectiveControl = ({ teamData }: { teamData: TeamOverallStats | null }) => {
   if (!teamData) return null;
@@ -10,52 +14,54 @@ const ObjectiveControl = ({ teamData }: { teamData: TeamOverallStats | null }) =
   const objectives = [
     {
       title: "Dragon",
-      icon: <GiMineExplosion />,
+      icon: <FaDragon className="text-white w-[25px] h-[25px]" />,
       iconBgColor: "bg-red bg-opacity-50",
       avg: teamData.avgDragons,
       controlPercent: teamData.dragonControlRate,
     },
+
     {
-      title: "Baron",
-      icon: <FaCoins />, // reuse same icon for barons
-      iconBgColor: "bg-orange bg-opacity-50",
-      avg: teamData.avgBarons,
-      controlPercent: teamData.firstBaronRate ?? 0,
+      title: "Atahkhan",
+      icon: <GiRose className="text-white w-[25px] h-[25px]" />,
+      iconBgColor: "bg-gradient-to-b from-red/80 to-blue/80 bg-opacity-50",
+      avg: teamData.avgAtahkhan,
+      controlPercent: 0,
     },
+    // {
+    //   title: "Inhibitor",
+    //   icon: <FaGem className="text-white w-[25px] h-[25px]" />,
+    //   iconBgColor: "bg-green bg-opacity-50",
+    //   avg: teamData.avgInhibitors,
+    //   controlPercent: teamData.firstInhibitorRate ?? 0,
+    // },
     {
       title: "Tower",
-      icon: <IoPieChart />, // reuse existing
+      icon: <GiChessRook className="text-white w-[25px] h-[25px]" />,
       iconBgColor: "bg-slate-500 bg-opacity-50",
       avg: teamData.avgTowers,
       controlPercent: teamData.firstTowerRate ?? 0,
     },
     {
-      title: "Inhibitor",
-      icon: <IoLogoGameControllerA />, // stand-in icon
-      iconBgColor: "bg-purple bg-opacity-50",
-      avg: teamData.avgInhibitors,
-      controlPercent: teamData.firstInhibitorRate ?? 0,
-    },
-    {
-      title: "Herald",
-      icon: <IoLogoGameControllerA />, // stand-in icon
-      iconBgColor: "bg-cyan bg-opacity-50",
-      avg: teamData.avgHeralds,
-      controlPercent: 0,
-    },
-    {
       title: "Grub",
-      icon: <IoLogoGameControllerA />, // stand-in icon
-      iconBgColor: "bg-green bg-opacity-50",
+      icon: <GiLeechingWorm className="text-white w-[25px] h-[25px]" />,
+      iconBgColor: "bg-purple bg-opacity-50",
       avg: teamData.avgGrubs,
       controlPercent: teamData.voidGrubControlRate ?? 0,
     },
     {
-      title: "Atahkhan",
-      icon: <IoLogoGameControllerA />, // stand-in icon
-      iconBgColor: "bg-yellow bg-opacity-50",
-      avg: teamData.avgAtahkhan,
+      title: "Herald",
+      icon: <GiSwirledShell className="text-white w-[25px] h-[25px]" />,
+      iconBgColor: "bg-purple bg-opacity-50",
+      avg: teamData.avgHeralds,
       controlPercent: 0,
+    },
+
+    {
+      title: "Baron",
+      icon: <GiCurledTentacle className="text-white w-[25px] h-[25px]" />,
+      iconBgColor: "bg-purple bg-opacity-50",
+      avg: teamData.avgBarons,
+      controlPercent: teamData.firstBaronRate ?? 0,
     },
   ];
 
@@ -67,11 +73,14 @@ const ObjectiveControl = ({ teamData }: { teamData: TeamOverallStats | null }) =
           key={obj.title}>
           <div className={`${obj.iconBgColor} p-1 rounded-md`}>{obj.icon}</div>
           <div className="text flex flex-col p-2 items-center sm:items-start truncate">
-            <h2 className="text-white/60 text-xl">{obj.title}{obj.title !== "Atahkhan" && 's'}</h2>
+            <h2 className="text-white/60 text-xl">
+              {obj.title}
+              {obj.title !== "Atahkhan" && "s"}
+            </h2>
             <p className="text-2xl">{obj.avg.toFixed(2)}</p>
             {obj.controlPercent > 0 && (
               <p className="text-sm text-white/60">
-                First {obj.title} Rate: {(obj.controlPercent).toFixed(0)}%
+                First {obj.title} Rate: {obj.controlPercent.toFixed(0)}%
               </p>
             )}
           </div>
