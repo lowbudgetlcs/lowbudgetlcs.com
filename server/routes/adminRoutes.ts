27720483e623c7b1eb03198b51097fe561e74b59
Mamii868 <marcmotenii@gmail.com>
2026-01-08 12:11:45 -0600

    Add basic session check route

--- server/routes/adminRoutes.ts
+++ server/routes/adminRoutes.ts
@@ -1,10 +1,16 @@
+import { fromNodeHeaders } from "better-auth/node";
 import cookieParser from "cookie-parser";
 import express, { Request, Response } from "express";
+import { auth } from "../utils/auth";
 
 const adminRoutes = express.Router();
 
 adminRoutes.use(cookieParser());
 
-// Better-auth placeholder
-// adminRoutes.post("/api/auth", ...);
+adminRoutes.get("/api/me", async (req: Request, res: Response) => {
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers)
+    });
+    return res.json(session);
+});
 export default adminRoutes;
