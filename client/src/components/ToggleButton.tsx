c6a06e7cb0b99893161e1d4dbd592fd5ba46c830
Mamii868 <marcmotenii@gmail.com>
2026-01-09 14:08:29 -0600

    Update css

--- client/src/components/ToggleButton.tsx
+++ client/src/components/ToggleButton.tsx
@@ -13,14 +13,10 @@ const ToggleButton = ({
 
   return (
     <label className="inline-flex items-center cursor-pointer scale-125">
-      <input
-        type="checkbox"
-        value=""
-        className="sr-only peer"
-        checked={isOn}
-        onChange={toggle}
-      />
-      <div className="relative w-11 h-6 bg-gray peer-focus:outline-hidden rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:rtl:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange"></div>
+      <input type="checkbox" value="" className="sr-only peer" checked={isOn} onChange={toggle} />
+      <div className="relative w-11 h-6 bg-gray peer-focus:outline-hidden rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full 
+      peer-checked:rtl:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5
+       after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange"></div>
     </label>
   );
 };
