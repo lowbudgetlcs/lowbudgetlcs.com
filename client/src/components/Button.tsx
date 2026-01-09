2251957e40e7e2441319c442aed746f9a607b7fa
Mamii868 <marcmotenii@gmail.com>
2026-01-09 14:47:34 -0600

    Update button css

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,11 +1,7 @@
 const Button = ({ children}: {children: any}) => {
     return (
-<div className="relative bg-blue/60 flex justify-center px-8 py-2 overflow-hidden group rounded-md">
-<span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-4 border-orange group-hover:w-full rounded-md ease"></span>
-<span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-4 border-orange group-hover:w-full ease"></span>
-<span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 border-l-4  border-orange group-hover:h-full ease"></span>
-<span className="absolute bottom-0 right-0 w-full h-0 transition-all duration-300 border-r-4 border-orange group-hover:h-full ease"></span>
-<span className="relative transition-colors duration-300 delay-200 text-white group-hover:text-white ease">{children}</span>
+<div className="relative bg-bg ring-2 ring-primary-light flex justify-center px-8 py-2 overflow-hidden group hover:bg-primary-dark transition duration-300 rounded-md">
+<span className="relative transition-colors duration-300 text-primary-light group-hover:text-white ease">{children}</span>
 </div>
     );
   };
