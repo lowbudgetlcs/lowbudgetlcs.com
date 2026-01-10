a2eb05dcfc7cb0b988e61c707a2dd6e95fd7661e
Mamii868 <marcmotenii@gmail.com>
2026-01-10 14:25:10 -0600

    Change css to match button

--- client/src/components/AltButton.tsx
+++ client/src/components/AltButton.tsx
@@ -1,6 +1,6 @@
 const AltButton = ({ children }: { children: any }) => {
   return (
-    <div className="relative bg-bg ring-2 ring-primary-light flex justify-center px-8 py-2 overflow-hidden group hover:bg-primary-dark transition duration-300 rounded-md">
+    <div className="relative bg-bg border border-primary-light cursor-pointer flex justify-center px-8 py-2 overflow-hidden group hover:bg-primary-dark transition duration-300 rounded-md">
       <span className="relative transition-colors duration-300 text-primary-light group-hover:text-white ease">
         {children}
       </span>
