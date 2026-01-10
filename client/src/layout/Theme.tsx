355850449dcfb9a368c0d1e0de296b0f56457678
Mamii868 <marcmotenii@gmail.com>
2026-01-10 11:19:50 -0600

    Add theme button

---
+++ client/src/layout/Theme.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useLocalStorageState } from "../hooks/uselocalStorageState";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const Theme = () => {
+  const [isLightMode, setIsLightMode] = useLocalStorageState("lightMode", false);
+
+  useEffect(() => {
+    const body = document.body;
+    if (isLightMode) {
+      body.classList.add("light");
+    } else {
+      body.classList.remove("light");
+    }
+  }, [isLightMode]);
+
+  return (
+    <div className="themeSelect text-2xl flex items-center justify-center text-text-primary">
+      {isLightMode ? (
+        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(false)}>
+          <FaSun/>
+        </button>
+      ) : (
+        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(true)}>
+          <FaMoon />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Theme;
