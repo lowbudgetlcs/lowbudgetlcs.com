b3defaf19c375e6d4ec8dbc946f57de23093669b
Mamii868 <marcmotenii@gmail.com>
2026-01-10 11:31:27 -0600

    Refactor hero section
    This is just going to be the background now. The summary will go over this to make it look more modern

--- client/src/features/Home/components/Hero.tsx
+++ client/src/features/Home/components/Hero.tsx
@@ -10,9 +10,9 @@ function Hero() {
     video = lblcsVideo;
   }
   return (
-    <div id="hero" className="hero xl:flex justify-center items-center relative w-full h-188 overflow-hidden">
+    <div id="hero" className="hero xl:flex justify-center items-center relative w-screen h-screen overflow-hidden">
       <video
-        className="-mt-6 heroVideo min-w-full min-h-160 lg:-mt-16 md:min-h-188 object-cover scale-110 lg:translate-y-8 top-0 left-0 z-1 "
+        className="-mt-6 heroVideo min-w-full min-h-160 lg:-mt-16 md:min-h-188 object-cover scale-125 lg:translate-y-8 top-0 left-0 z-1 "
         autoPlay
         muted
         playsInline
@@ -20,9 +20,7 @@ function Hero() {
       >
         <source src={video} type="video/mp4"></source>
       </video>
-      <div className="overlay w-full h-88 md:h-64 bg-linear-to-b from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute bottom-0 left-0 z-2"></div>
-      <div className="hidden md:block overlay w-40 h-full bg-linear-to-l from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute top-0 left-0 z-2"></div>
-      <div className="hidden md:block overlay w-40 h-full bg-linear-to-r from-transparent dark:from-transparent via-white dark:via-black dark:to-black to-[#F4F5FB] opacity-100 scale-110 absolute top-0 right-0 z-2"></div>
+      <div className="overlay w-full h-full bg-radial from-transparent via-transparent to-bg-dark absolute top-0 left-0 z-2"></div>
     </div>
   );
 }
