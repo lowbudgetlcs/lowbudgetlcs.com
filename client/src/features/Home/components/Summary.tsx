b8e5b69b34d1b1ad23ec407ba99cfd48ac19ab52
Mamii868 <marcmotenii@gmail.com>
2026-01-11 10:29:44 -0600

    Remove unused code

--- client/src/features/Home/components/Summary.tsx
+++ client/src/features/Home/components/Summary.tsx
@@ -1,7 +1,3 @@
-import HomeCard from "./HomeCard";
-import aboutImage from "../../../assets/summaryImg.jpg";
-import twitchImage from "../../../assets/twitchPhone.jpg";
-import discordImage from "../../../assets/smilingGamer.jpg";
 import Button from "../../../components/Button";
 import AltButton from "../../../components/AltButton";
 import { Link, NavLink } from "react-router-dom";
@@ -9,38 +5,6 @@ import SubdomainLink from "../../../components/SubdomainLink";
 import { FaDiscord, FaInstagram, FaTwitch, FaYoutube } from "react-icons/fa";
 
 function Summary() {
-  // const aboutCard = {
-  //   title: "Amateurs Unite!",
-  //   text: `The LowBudget LCS is a competitive, free, and friendly amateur rec
-  //             league. We have multiple leagues that range across all skill levels. We
-  //             provide an experience that mirrors what would happen if Riot Games
-  //             suffered a catastrophic financial disaster.`,
-  //   link: "/about",
-  //   btnText: "About Us",
-  //   image: aboutImage,
-  // };
-
-  // const twitchCard = {
-  //   title: "Watch Games Live",
-  //   text: `Watch matches from all brackets live on Twitch! Cheer on your
-  //             favorite team as they win big (or int big) while interacting with
-  //             both the casters and community. Earn all the Schmeckles you can!`,
-  //   link: "https://www.twitch.tv/lowbudgetlcs",
-  //   btnText: "Watch Here",
-  //   image: twitchImage,
-  // };
-
-  // const discordCard = {
-  //   title: "Have a Blast with Friends",
-  //   text: `The LBLCS is a community where players of all ranks can meet
-  //             awesome people, play organized League of Legends, and find a
-  //             welcome environment for just about everything! Show off your pets,
-  //             share cool memes, and talk it up with the community!`,
-  //   link: "https://discord.com/invite/XfsEEq4aPC",
-  //   btnText: "Join The Discord",
-  //   image: discordImage,
-  // };
-
   const slogans: { top: string; bottom: string }[] = [
     { top: "The League Where Your Mom", bottom: "Can Be Your Coach." },
     { top: "Competitive Enough to Crash Out.", bottom: "Casual Enough to Have Fun." },
