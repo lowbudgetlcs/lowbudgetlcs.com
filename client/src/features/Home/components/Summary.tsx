a66f9e55a39d97d8bcd7ae3838d000816d8c9438
Mamii868 <marcmotenii@gmail.com>
2026-01-12 09:13:28 -0600

    Add new slogan math

--- client/src/features/Home/components/Summary.tsx
+++ client/src/features/Home/components/Summary.tsx
@@ -3,15 +3,18 @@ import AltButton from "../../../components/AltButton";
 import { Link, NavLink } from "react-router-dom";
 import SubdomainLink from "../../../components/SubdomainLink";
 import { FaDiscord, FaInstagram, FaTwitch, FaYoutube } from "react-icons/fa";
+import { useState } from "react";
 
 function Summary() {
-  const slogans: { top: string; bottom: string }[] = [
-    { top: "The League Where Your Mom", bottom: "Can Be Your Coach." },
-    { top: "Competitive Enough to Crash Out.", bottom: "Casual Enough to Have Fun." },
-    { top: "Find Your Team, Lose Your Lane,", bottom: "Keep Your Friends." },
-    { top: "Where The Production Value", bottom: "Is as Low as Our Win Rates." },
-  ];
-  const slogan = slogans[Math.floor(Math.random() * slogans.length)];
+  const [slogan] = useState(() => {
+    const slogans: { top: string; bottom: string }[] = [
+      { top: "The League Where Your Mom", bottom: "Can Be Your Coach." },
+      { top: "Competitive Enough to Crash Out.", bottom: "Casual Enough to Have Fun." },
+      { top: "Find Your Team, Lose Your Lane,", bottom: "Keep Your Friends." },
+      { top: "Where The Production Value", bottom: "Is as Low as Our Win Rates." },
+    ];
+    return slogans[Math.floor(Math.random() * slogans.length)];
+  });
   return (
     <div className="summary absolute top-0 left-0 w-full h-full flex justify-center md:grid md:grid-cols-2 gap-4 place-items-center items-center bg-linear-90 from-black to-transparent">
       <div className="titleContent flex flex-col gap-4 max-w-xl rounded-md p-6">
@@ -28,7 +31,9 @@ function Summary() {
         </div>
 
         <div className="flex gap-4 justify-center opacity-0 animate-slide-in-900">
+          <NavLink to="https://discord.com/invite/XfsEEq4aPC" target="_blank">
           <Button className="discord">Join The Discord</Button>
+          </NavLink>
           <Link to="/about">
             <AltButton>About Us</AltButton>
           </Link>
@@ -45,6 +50,7 @@ function Summary() {
             <NavLink
               aria-label="Instagram Profile"
               target="_blank"
+              rel="noopener noreferrer"
               className={``}
               to="https://www.instagram.com/lowbudgetlcs">
               <Button className="text-2xl">
@@ -52,9 +58,10 @@ function Summary() {
               </Button>
             </NavLink>
             <NavLink
-              aria-label="Discord Server"
-              target="_blank"
+                aria-label="Discord Server"
+                target="_blank"
               to="https://discord.com/invite/XfsEEq4aPC"
+              rel="noopener"
               className={``}>
               <Button className="text-2xl">
                 <FaDiscord />
@@ -64,6 +71,7 @@ function Summary() {
               aria-label="Twitch Channel"
               target="_blank"
               to="https://www.twitch.tv/lowbudgetlcs"
+              rel="noopener"
               className={``}>
               <Button className="text-2xl">
                 <FaTwitch />
@@ -73,6 +81,7 @@ function Summary() {
               aria-label="Youtube Channel"
               target="_blank"
               to="https://www.youtube.com/@lowbudgetlcs9513"
+              rel="noopener"
               className={``}>
               <Button className="text-2xl">
                 <FaYoutube />
