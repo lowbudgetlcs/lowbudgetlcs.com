f856b47a1688be8714ad47ea43f86d8018f76a95
Mamii868 <marcmotenii@gmail.com>
2026-01-12 09:17:08 -0600

    Add screen reader compatibility

--- client/src/layout/Theme.tsx
+++ client/src/layout/Theme.tsx
@@ -17,11 +17,11 @@ const Theme = () => {
   return (
     <div className="themeSelect text-2xl flex items-center justify-center text-text-primary">
       {isLightMode ? (
-        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(false)}>
+        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(false)} aria-label="Switch to dark mode">
           <FaSun/>
         </button>
       ) : (
-        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(true)}>
+        <button className="cursor-pointer p-2" onClick={() => setIsLightMode(true)} aria-label="Switch to light mode">
           <FaMoon />
         </button>
       )}
