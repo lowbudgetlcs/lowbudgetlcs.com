cc373f45109dd3d9cd506c14d053deeeb542ddad
Mamii868 <marcmotenii@gmail.com>
2026-02-10 15:53:57 -0600

    Fix positioning on large screens

--- client/src/features/MatchHistory/components/PlayerContainers/PlayerContainer.tsx
+++ client/src/features/MatchHistory/components/PlayerContainers/PlayerContainer.tsx
@@ -18,8 +18,8 @@ const PlayerContainer = ({ playerData, allPlayers, activeLink }: PlayerContainer
     killParticipation = Number(rawKP.toFixed(0));
   }
   return (
-    <div className="playerContainer bg-light-gray p-2 h-22.5 w-max grow rounded-md">
-      <div className="champPlayerInfo flex gap-2 items-center h-full">
+    <div className="playerContainer bg-bg-light light:bg-bg-dark p-2 rounded-md border border-border">
+      <div className="champPlayerInfo flex items-center justify-between">
         <PlayerInfo playerData={playerData} />
         {activeLink === "Loadout/KDA" ? (
           <>
@@ -31,32 +31,21 @@ const PlayerContainer = ({ playerData, allPlayers, activeLink }: PlayerContainer
               <ItemDisplay playerData={playerData} />
             </div>
             <div className="kda flex flex-col items-center shrink-0 w-28">
-              <div className="flex text-2xl font-bold justify-center">
+              <div className="flex font-bold justify-center text-lg">
                 <p>{playerData.kills}</p>&nbsp;/&nbsp;
                 <p className="text-red">{playerData.deaths}</p>
                 &nbsp;/&nbsp;
                 <p>{playerData.assists}</p>
               </div>
-              <p className="text-white/60">K / D / A</p>
+              <p
+                className={`font-bold text-sm ${
+                  killParticipation > 50 ? "text-green" : killParticipation < 50 && killParticipation > 30 ? "text-yellow" : "text-red"
+                }`}>
+                {killParticipation}% KP
+              </p>
             </div>
             <div className="flex flex-col text-center shrink-0 w-28">
-              <p className="cs text-2xl font-bold">
-                {playerData.totalMinionsKilled + playerData.neutralMinionsKilled}
-              </p>
-              <p className="text-white/60">CS</p>
-            </div>
-            <div className="flex flex-col text-center shrink-0 w-32 ">
-              <p
-                className={`text-2xl font-bold ${
-                  killParticipation > 50
-                    ? "text-green"
-                    : killParticipation < 50 && killParticipation > 30
-                    ? "text-yellow"
-                    : "text-red"
-                }`}>
-                {killParticipation}%
-              </p>
-              <p className="text-white/60">Kill Participation</p>
+              <p className="cs font-bold">{playerData.totalMinionsKilled + playerData.neutralMinionsKilled} <span className="text-text-secondary">CS</span></p>
             </div>
           </>
         ) : (
--- client/src/features/MatchHistory/components/TeamContainer.tsx
+++ client/src/features/MatchHistory/components/TeamContainer.tsx
@@ -6,25 +6,25 @@ import StatList from "./PlayerContainers/StatList";
 
 const TeamContainer = ({team, teamPlayers, activeLink}: {team: TeamDto, teamPlayers: ParticipantDto[], activeLink: string}) => {
   return (
-    <div className="team1Container md:w-1/2 bg-bg border border-border rounded-md p-4 flex flex-col gap-4">
-      <div className="teamTitle flex gap-2 items-center p-4 md:p-0">
+    <div className="team1Container lg:w-1/2 bg-bg border border-border rounded-md p-4 flex flex-col gap-2">
+      <div className="teamTitle flex gap-2 items-end p-4 md:p-0">
         <h3 className={`${team.win ? "text-green brightness-125" : "text-red"} font-bold text-2xl`}>{team.win ? "Victory" : "Defeat"}</h3>
-        <p className="text-white/60">{team.teamId === 100 ? "Blue" : "Red"} Team</p>
+        <p className="text-white/60 text-sm">{team.teamId === 100 ? "Blue" : "Red"} Team</p>
       </div>
       <Objectives team={team} />
       <div></div>
       {activeLink === "Loadout/KDA" ? (
-        <div className="playerContainer flex flex-col gap-2 w-screen md:w-[80vw] lg:w-full overflow-x-scroll no-scrollbar">
+        <div className="playerContainer flex flex-col gap-2 w-full overflow-x-scroll no-scrollbar">
           {teamPlayers.map((player) => (
             <PlayerContainer key={player.puuid} playerData={player} allPlayers={teamPlayers} activeLink={activeLink} />
           ))}
         </div>
       ) : activeLink === "Graphs" ? (
-        <div className="playerContainer flex flex-col gap-2 w-screen md:w-[80vw] lg:w-full">
+        <div className="playerContainer flex flex-col gap-2 w-full">
           <DamageContainer players={teamPlayers} />
         </div>
       ) : (
-        <div className="statList flex flex-col gap-2 w-screen md:w-[80vw] lg:w-full overflow-x-scroll no-scrollbar">
+        <div className="statList flex flex-col gap-2 w-full overflow-x-scroll no-scrollbar">
           <StatList players={teamPlayers} />
         </div>
       )}
