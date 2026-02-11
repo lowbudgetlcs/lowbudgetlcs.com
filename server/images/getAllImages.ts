2821c0b269617e24c870c28d7577b33558b67d4c
Mamii868 <marcmotenii@gmail.com>
2026-02-11 12:39:03 -0600

    Add failsafes

--- server/images/getAllImages.ts
+++ server/images/getAllImages.ts
@@ -1,7 +1,7 @@
 import { imageCache, storeAllImages, lastCacheTime } from "./storeAllImages";
 
 // 24 hours in milliseconds
-const CACHE_TTL = 24 * 60 * 60 * 1000;
+const cacheTtl = 24 * 60 * 60 * 1000;
 
 interface ChampionData {
   id: number;
@@ -12,29 +12,43 @@ interface ChampionData {
   squarePortraitPath: string;
   roles: string[];
 }
+
+let championJson: ChampionData[] | null = null;
+let lastCache = 0;
+
 export const getAllImages = async (championName: string, imageType: string, championId?: number) => {
   const currentTime = Date.now();
 
-  // If cache is stale or empty, fetch and store images
-  if (imageCache.size === 0 || currentTime - lastCacheTime >= CACHE_TTL) {
+  if (imageCache.size === 0 || currentTime - lastCacheTime >= cacheTtl) {
     await storeAllImages();
+    championJson = null;
   }
-    let championJson: ChampionData[];
 
   if (championId) {
-    const response = await fetch("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json");
-    championJson = await response.json();
-    const championData = championJson.find((champ) => champ.id === championId);
-    if (championData) {
-      championName = championData.alias.toLowerCase();
-    } else {
-      console.error(`Champion with ID ${championId} not found in champion summary.`);
+    if (!championJson || currentTime - lastCache >= cacheTtl) {
+      try {
+        const response = await fetch("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json");
+        if (response.ok) {
+          championJson = await response.json();
+          lastCache = currentTime;
+        }
+      } catch (error) {
+        console.error("[Get Images] Failed to fetch champion Json:", error);
+      }
+    }
+
+    if (championJson) {
+      const championData = championJson.find((champ) => champ.id === championId);
+      if (championData) {
+        championName = championData.alias;
+      } else {
+        console.error(`[Get Images] Champion with ID ${championId} not found in champion Json.`);
+      }
     }
   }
-  
+
   let championImages = imageCache.get(championName);
 
-  // Case-insensitive lookup if direct lookup fails
   if (!championImages) {
     const lowerName = championName.toLowerCase();
     for (const [key, val] of imageCache.entries()) {
