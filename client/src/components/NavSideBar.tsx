cf904a14eb708a4be880fd4520e5b5d4435cf0a4
Mamii868 <marcmotenii@gmail.com>
2026-02-16 23:36:10 -0600

    fIx underlines

--- client/src/components/NavSideBar.tsx
+++ client/src/components/NavSideBar.tsx
@@ -1,15 +1,15 @@
 interface NavListProps {
-  activeLink: number | undefined;
-  toggleActive: (navItem: number) => void;
-  navItems: Array<number>;
+  activeLink: string | number | undefined;
+  toggleActive: (navItem: string | number) => void;
+  navItems: Array<string | number>;
   prefix?: string;
 }
 
 const NavSideBar: React.FC<NavListProps> = ({ activeLink, toggleActive, navItems, prefix }) => {
   let currentAnimationNum = 200;
   return (
-    <div className="sidebar text-white pt-20 bg-light-gray transition duration-500 opacity-0 animate-slide-in-right text-nowrap">
-      <ul className="flex flex-row md:flex-col md:justify-center items-center md:px-8 gap-8 text-lg md:text-2xl overflow-x-scroll overflow-y-hidden md:overflow-y-scroll font-bold no-scrollbar p-2">
+    <div className="sidebar text-text-secondary pt-20 bg-bg transition duration-500 opacity-0 animate-slide-in-right text-nowrap">
+      <ul className="flex flex-row md:flex-col md:justify-center md:px-8 gap-8 md:text-2xl overflow-x-scroll overflow-y-hidden md:overflow-y-scroll font-bold no-scrollbar p-2">
         {navItems.map((navItem) => {
           currentAnimationNum = currentAnimationNum + 100;
           const animationTiming = `animate-slide-in-${currentAnimationNum}`;
@@ -17,12 +17,12 @@ const NavSideBar: React.FC<NavListProps> = ({ activeLink, toggleActive, navItems
             <li
               key={navItem}
               onClick={() => toggleActive(navItem)}
-              className={`hover:cursor-pointer hover:text-orange transition duration-300 opacity-0 ${animationTiming}`}>
-              {prefix}{" "}{navItem}
+              className={`relative inline-flex w-fit pb-1 hover:cursor-pointer hover:text-orange transition duration-300 opacity-0 ${animationTiming}`}>
+              {prefix} {navItem}
               <span
                 className={`line absolute ${
                   activeLink === navItem ? "w-full" : "w-0"
-                } h-0 transition-all duration-200 border-b-4 border-orange rounded-md bg-orange -bottom-0.5 left-0`}></span>
+                } transition-all duration-200 border-b-4 border-orange rounded-md bottom-0 left-0`}></span>
             </li>
           );
         })}
