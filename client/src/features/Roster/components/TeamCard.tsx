eeffc5d1cfb7e0dfd96bdcf8ab6ab79b703fa29e
Mamii868 <marcmotenii@gmail.com>
2026-02-24 13:03:01 -0600

    Add Icon for no icon

--- client/src/features/Roster/components/TeamCard.tsx
+++ client/src/features/Roster/components/TeamCard.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { PlayerProps } from "../../../leagueData";
 import TeamDropdown from "./TeamDropdown";
-
+import lblcsIcon from "../../../assets/icons/lblcsIcon.svg";
 interface TeamProps {
   teamName: string;
   division: string;
@@ -86,33 +86,33 @@ function TeamCard({ teamName, logo, playerList, isOpen, onToggle }: TeamProps) {
 
   const displayLogo = () => {
     if (logo) {
-      return <img src={logo} className="logo shrink-0 w-20 text-center text-3xl h-20" />;
+      return <img src={logo} className="logo shrink-0 w-20 text-center text-xl h-20" />;
     } else {
-      return <div className="logo shrink-0 w-20 text-center text-3xl h-20 bg-bg">No logo 😢</div>;
+      return <img src={lblcsIcon} className="logo shrink-0 w-20 text-center grayscale opacity-60 text-xl h-20" />;
     }
   };
 
   return (
     <div
       ref={cardRef}
-      className={`teamCard relative transition duration-300 ${isOpen ? "rounded-t-xl" : "rounded-xl"} bg-bg-light border-border border cursor-pointer group`}>
-      <div onClick={togglePlayerList} className="p-2">
+      className={`teamCard relative transition duration-300 ${isOpen ? "rounded-t-xl" : "rounded-xl"} bg-bg border-border border group w-full`}>
+      <div onClick={togglePlayerList} className="p-2 cursor-pointer">
         <div className="dropBtn absolute bottom-0 right-0 self-end">
           <div className="burger cursor-pointer relative h-12 w-12 gap-1 hover:cursor-pointer self-baseline">
             <div
               className={`absolute ${
                 isOpen ? "-rotate-45" : "rotate-45"
-              } top-4 left-0 transition-all duration-500 px-3 py-0.5 rounded-xl bg-text-primary`}></div>
+              } top-4 left-0 transition-all duration-300 px-3 py-0.5 rounded-xl bg-text-primary`}></div>
             <div
               className={`absolute ${
                 isOpen ? "rotate-45" : "-rotate-45"
-              } top-4 left-4 transition-all duration-500 px-3 py-0.5 rounded-xl bg-text-primary`}></div>
+              } top-4 left-4 transition-all duration-300 px-3 py-0.5 rounded-xl bg-text-primary`}></div>
           </div>
         </div>
         <div className="flex flex-col md:flex-row py-4 md:py-0 gap-4 items-center min-h-20 overflow-hidden">
           <div className="logoContainer flex flex-col md:flex-row gap-4 w-full md:w-auto md:h-full items-center">
             {displayLogo()}
-            <div className={`w-full h-3 md:w-3 md:h-full bg-orange`}></div>
+            <div className={`w-full h-1 md:w-1 md:h-16 bg-orange`}></div>
           </div>
           <div className="flex flex-col md:flex-row flex-1 shrink md:ml-4 items-center">
             <h3 className="teamName text-xl text-center md:text-left font-semibold px-16 md:px-8">{teamName}</h3>
