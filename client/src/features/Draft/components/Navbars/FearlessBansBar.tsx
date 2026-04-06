b2ae6b5a4f718ecc4dcd3cf74a3f4b7e3df25776
Mamii868 <marcmotenii@gmail.com>
2026-04-06 18:17:29 -0500

    Add organized games

--- client/src/features/Draft/components/Navbars/FearlessBansBar.tsx
+++ client/src/features/Draft/components/Navbars/FearlessBansBar.tsx
@@ -1,73 +1,84 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useFearlessContext } from "../../providers/FearlessProvider";
+import lblcsIcon from "../../../../assets/icons/lblcsIcon.svg";
 
 const FearlessBansBar = () => {
   const { fearlessState } = useFearlessContext();
   const [barIsOpen, setBarIsOpen] = useState<boolean>(false);
-  const FearlessBarRef = useRef<any>(null)
+  const FearlessBarRef = useRef<any>(null);
   const handleClickOutside = useCallback((e: MouseEvent) => {
     if (FearlessBarRef.current && !FearlessBarRef.current.contains(e.target)) {
       setBarIsOpen(false);
     }
   }, []);
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside, true);
-    return () => document.removeEventListener('mousedown', handleClickOutside, true)
-  },[handleClickOutside])
-  
+    document.addEventListener("mousedown", handleClickOutside, true);
+    return () => document.removeEventListener("mousedown", handleClickOutside, true);
+  }, [handleClickOutside]);
+
   if (!fearlessState) {
     return null;
   }
+
+  const loopThroughCompletedDrafts = () => {
+    const picksByGame: Array<Array<string>> = [];
+    for (let i = 1; i <= fearlessState.completedDrafts; i++) {
+      const picksToGet = i * 5;
+      const team1Picks = fearlessState.team1Picks.slice(picksToGet - 5, picksToGet);
+      const team2Picks = fearlessState.team2Picks.slice(picksToGet - 5, picksToGet);
+      picksByGame.push([...team1Picks, ...team2Picks]);
+    }
+    return picksByGame;
+  };
   return (
     <>
-      <div ref={FearlessBarRef} className="fearlessBans group relative flex flex-col items-center justify-center">
+      <div
+        ref={FearlessBarRef}
+        onClick={() => setBarIsOpen(!barIsOpen)}
+        className="previousFearlessPicks absolute bottom-0 w-full bg-bg border border-border flex flex-col justify-center-center hover:cursor-pointer hover:bg-bg-light transition duration-150 group">
+        <p className="text-xl text-text-secondary font-bold pl-2 group-hover:text-text-primary">Previous Fearless Picks</p>
         <div
-          onClick={() => setBarIsOpen(!barIsOpen)}
-          className="champContainerPullup absolute -top-6 w-12 h-6 bg-black border-white/80 border-x-2 border-t-2 flex items-center rounded-t-md hover:cursor-pointer"
-        >
-          <div
-            className={`line w-8 h-2 bg-white rounded translate-x-1 transition duration-300 ${
-              barIsOpen ? "rotate-30" : "rotate-[-30deg]"
-            } `}
-          ></div>
-          <div
-            className={`line w-8 h-2 bg-white rounded -translate-x-1 transition duration-300 ${
-              barIsOpen ? "rotate-[-30deg]" : "rotate-30"
-            }  `}
-          ></div>
-        </div>
-        <div
-          className={`absolute hidden group-hover:animate-fadeIn-300ms group-hover:flex -top-14 w-28 text-center items-center justify-center rounded-md border-2 border-gray font-bold h-6 bg-light-gray`}
-        >
-          <p className="text-sm">Previous Picks</p>
-        </div>
-        {barIsOpen && (
-          <div className="absolute bottom-8 min-w-[64vw] lg:min-w-[50vw] xl:min-w-64 min-h-24 pb-4 pt-2 px-2 z-20
-          bg-black border-2 border-gray rounded-md shadow-2xl flex flex-col gap-2 items-center justify-center animate-fadeIn-300ms">
-            <p className="font-bold text-lg">Previous Picks</p>
-            <div className="bansBox grid grid-cols-10 gap-2">
-              {fearlessState.allPicks.map((ban, index) => {
-                const fixedName =
-                  ban.toLowerCase() === "wukong" ? "monkeyKing" : ban;
-                const imageURL = `${import.meta.env.VITE_BACKEND_URL}/images/api/champion/${fixedName}/splashTile`;
-                return (
-                  <div
-                    className={`banBox ${ban} w-12 h-12 border-2 border-gray rounded-md`}
-                    key={index}
-                  >
-                    {ban !== "nothing" && (
+          className={`w-full ${barIsOpen ? "" : "h-0"} z-20 border-x border-b border-border bg-bg rounded-md shadow-2xl flex flex-col gap-2 items-center justify-center`}>
+          {loopThroughCompletedDrafts().map((gamePicks, index) => {
+            return (
+              <div key={`Game` + index} className="flex gap-1 items-center justify-center w-full text-text-primary">
+                <div className="blueSide flex gap-2">
+                  {gamePicks.slice(0, 5).map((pick, index) => {
+                    return pick === "nothing" ? (
+                      <img key={pick + index} src={lblcsIcon} alt={`${pick}`} className="w-12 h-12 border border-border grayscale opacity-50" />
+                    ) : (
                       <img
-                        src={imageURL}
-                        alt={ban !== "nothing" ? ban : "Nothing"}
-                        className={`w-full h-full object-cover grayscale-40`}
+                        key={pick + index}
+                        src={`${import.meta.env.VITE_BACKEND_URL}/images/api/champion/${pick === "Wukong" ? "MonkeyKing" : pick}/square`}
+                        alt={`${pick}`}
+                        className="w-12 h-12"
                       />
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
+                    );
+                  })}
+                </div>
+                <p>Game {index + 1}</p>
+                <div className="redSide flex gap-2">
+                  {gamePicks.slice(5, 10).map((pick, index) => {
+                    return pick === "nothing" ? (
+                      <img key={pick + index} src={lblcsIcon} alt={`${pick}`} className="w-12 h-12 border border-border grayscale opacity-50" />
+                    ) : (
+                      <img
+                        key={pick + index}
+                        src={`${import.meta.env.VITE_BACKEND_URL}/images/api/champion/${pick === "Wukong" ? "MonkeyKing" : pick}/square`}
+                        alt={`${pick}`}
+                        className="w-12 h-12"
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        className={`absolute hidden group-hover:animate-fadeIn-300ms group-hover:flex -top-14 w-28 text-center items-center justify-center rounded-md border-2 border-gray font-bold h-6 bg-light-gray`}>
+        <p className="text-sm">Previous Picks</p>
       </div>
     </>
   );
