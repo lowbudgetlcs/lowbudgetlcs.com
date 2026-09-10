688c680e18174b36b8cfecf2ef3cbf50e81a8123
Mamii868 <marcmotenii@gmail.com>
2026-09-09 19:33:33 -0500

    Add sockets

--- server/features/draft/draftHandlers/fixHandlers/handleFixes.ts
+++ server/features/draft/draftHandlers/fixHandlers/handleFixes.ts
@@ -1,16 +1,16 @@
 import { requestFixProps } from "../../types/draftInterfaces";
 import requestFix from "./requestFix";
-import { Namespace } from "socket.io";
+import { Namespace, Socket } from "socket.io";
 import { DraftStateProps } from "../../models/draftState";
 
 // Handles fix requests AND responses from both sides and keeps track of which side has requested a fix.
 // If a fix request is open for one side, this should not allow another one come in from the same side.
 // Does use sideCode validation but scrubs that from the response before emitting it to the opposing side.
-const handleFixes = async (io: Namespace, getDraftState: (lobbyCode: string) => DraftStateProps | null) => {
+const handleFixes = async (socket: Socket, getDraftState: (lobbyCode: string) => DraftStateProps | null) => {
   let didRedRequestFix = false;
   let didBlueRequestFix = false;
 
-  io.on("fixRequest", async (data: requestFixProps) => {
+  socket.on("fixRequest", async (data: requestFixProps) => {
 
     const currentDraftState = getDraftState(data.lobbyCode);
 
@@ -24,24 +24,24 @@ const handleFixes = async (io: Namespace, getDraftState: (lobbyCode: string) => 
     if (data.sideRequesting === currentDraftState.redUser && !didRedRequestFix) {
       didRedRequestFix = true;
 
-      const response = await requestFix({ ...data, io });
+      const response = await requestFix({ ...data, socket });
 
       //   Changes sideRequesting to "red" instead of the sideCode
       //   Emits answer from opposing side
       response.sideRequesting = "red";
-      io.to(data.lobbyCode).emit("fixResponse", response);
+      socket.to(data.lobbyCode).emit("fixResponse", response);
       didRedRequestFix = false;
 
       //   Checks if blue side is requesting a fix and there isn't one pending
     } else if (data.sideRequesting === currentDraftState.blueUser && !didBlueRequestFix) {
       didBlueRequestFix = true;
 
-      const response = await requestFix({ ...data, io });
+      const response = await requestFix({ ...data, socket });
 
       //   Changes sideRequesting to "blue" instead of the sideCode
       //   Emits answer from opposing side
       response.sideRequesting = "blue";
-      io.to(data.lobbyCode).emit("fixResponse", response);
+      socket.to(data.lobbyCode).emit("fixResponse", response);
       didBlueRequestFix = false;
     }
   });
--- server/features/draft/draftHandlers/fixHandlers/requestFix.ts
+++ server/features/draft/draftHandlers/fixHandlers/requestFix.ts
@@ -3,8 +3,8 @@ import { fixProps, requestFixProps } from "../../types/draftInterfaces";
 
 
 // Sends a request to fix a champion pick and waits for the response from the other side.
-const requestFix = async ({ sideRequesting, sourceChampion, replacementChampion, replacementSource, io, lobbyCode }: requestFixProps) => {
-    io.to(lobbyCode).emit("requestFix", {
+const requestFix = async ({ sideRequesting, sourceChampion, replacementChampion, replacementSource, socket, lobbyCode }: requestFixProps) => {
+    socket.to(lobbyCode).emit("requestFix", {
         sideRequesting,
         sourceChampion,
         replacementChampion,
@@ -13,7 +13,7 @@ const requestFix = async ({ sideRequesting, sourceChampion, replacementChampion,
 
     // Returns the above & status: boolean (true if the fix was accepted, false otherwise)
     return new Promise<fixProps>((resolve) => {
-        io.once("fixResponse", (response: fixProps) => {
+        socket.once("fixResponse", (response: fixProps) => {
             resolve(response);
         });
     });
